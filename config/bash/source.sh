9ef749154dda1af449cdeb794f8dea314205f940
xenodium <alvaro@xenodium.com>
2014-07-27 14:05:16 +0100

    - Renames install file to source (that's it's purpose). - Adds colors bash variables.

--- config/bash/source.sh
+++ config/bash/source.sh
@@ -2,8 +2,6 @@
 set -o nounset
 set -o errexit
 
-cp -i bashrc_overrides ~/.bashrc_overrides
-
 if ! grep -q 'bashrc_overrides' ~/.bashrc; then
  echo 'adding .bashrc_overrides to .bashrc'
  echo 'source ~/.bashrc_overrides' >> ~/.bashrc
