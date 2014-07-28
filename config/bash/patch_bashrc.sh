5e23946ce7f910f42b6a3e54627a09d6bbf14879
xenodium <alvaro@xenodium.com>
2014-07-28 19:13:56 +0100

    Few fixes to dotfiles and cleanup.

--- config/bash/patch_bashrc.sh
+++ config/bash/patch_bashrc.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
-set -o nounset
-set -o errexit
 
 if ! grep -q 'bashrc_overrides' ~/.bashrc; then
  echo 'adding .bashrc_overrides to .bashrc'
  echo 'source ~/.bashrc_overrides' >> ~/.bashrc
 fi
-source ~/.bashrc
+
