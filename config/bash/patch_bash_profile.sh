60c688a6eb3da0df68717dcb9654765774d21842
xenodium <alvaro@xenodium.com>
2014-07-31 23:09:05 +0100

    s/exit/return

--- config/bash/patch_bash_profile.sh
+++ config/bash/patch_bash_profile.sh
@@ -3,7 +3,7 @@
 echo 'adding .bash_overrides to .bash_profile'
 if [[ ! -e ~/.bash_profile  ]]; then
   echo 'source ~/.bash_overrides' >> ~/.bash_profile
-  return 0
+  exit 0
 fi
 
 if ! grep -q 'bash_overrides' ~/.bash_profile; then
