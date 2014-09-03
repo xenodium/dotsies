d3cf6b01ddbf8f4e95f6f955da36be1768fd9424
alvaro <alvaro@xenodium.com>
2014-09-04 00:43:21 +0100

    Cleaner modeline.

--- emacs/init.el
+++ emacs/init.el
@@ -195,31 +195,32 @@
 (setq-default mode-line-format
       (list
        ;; the buffer name; the file name as a tool tip
-       '(:eval (propertize "%b " 'face 'font-lock-keyword-face
+       '(:eval (propertize "%b"
+                           'face 'font-lock-keyword-face
                            'help-echo (buffer-file-name)))
-
-       ;; line and column
-       "(" ;; '%02' to set to 2 chars at least; prevents flickering
-       (propertize "%02l" 'face 'font-lock-type-face) ","
+       " | "
+       ;; line and column, '%02' to set to 2 chars at least
+       ;; prevents flickering
+       (propertize "%02l" 'face 'font-lock-type-face)
+       ","
        (propertize "%02c" 'face 'font-lock-type-face)
-       ") "
+       " | "
 
        ;; relative position, size of file
-       "["
        (propertize "%p" 'face 'font-lock-constant-face) ;; % above top
        "/"
        (propertize "%I" 'face 'font-lock-constant-face) ;; size
-       "] "
+       " | "
 
        ;; the current major mode for the buffer.
-       "["
-
-       '(:eval (propertize "%m" 'face 'font-lock-string-face
+       '(:eval (propertize "%m"
+                           'face
+                           'font-lock-string-face
                            'help-echo buffer-file-coding-system))
-       "] "
+       " | "
 
 
-       "[" ;; insert vs overwrite mode, input-method in a tooltip
+       ;; insert vs overwrite mode, input-method in a tooltip
        '(:eval (propertize (if overwrite-mode "Ovr" "Ins")
                            'face 'font-lock-preprocessor-face
                            'help-echo (concat "Buffer is in "
@@ -236,17 +237,16 @@
                  (concat ","  (propertize "RO"
                                           'face 'font-lock-type-face
                                           'help-echo "Buffer is read-only"))))
-       "] "
+       " | "
 
        ;; add the time, with the date and the emacs uptime in the tooltip
        '(:eval (propertize (format-time-string "%H:%M")
                            'help-echo
                            (concat (format-time-string "%c; ")
                                    (emacs-uptime "Uptime:%hh"))))
-       " --"
        ;; i don't want to see minor-modes; but if you want, uncomment this:
        ;; minor-mode-alist  ;; list of minor modes
-       "%-" ;; fill with '-'
+       "% " ;; fill with '-'
        ))
 
 ;; Highlight lines longer than 100 columns.
