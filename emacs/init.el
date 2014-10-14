12d2874356b24e6d1f9e752c0d8aedc055030683
xenodium <alvaro@xenodium.com>
2014-10-14 12:56:38 +0100

    Removing unnecessary binding.

--- emacs/init.el
+++ emacs/init.el
@@ -95,7 +95,6 @@
 (global-set-key (kbd "M-C-s") 'helm-multi-swoop-all)
 (global-set-key (kbd "C-r") nil)
 (global-set-key (kbd "C-c i") 'helm-imenu)
-(global-set-key (kbd "M-C-s") 'helm-swoop)
 (define-key helm-map (kbd "<tab>") 'helm-execute-persistent-action) ; rebihnd tab to do persistent action
 (define-key helm-map (kbd "C-i") 'helm-execute-persistent-action) ; make TAB works in terminal
 (define-key helm-map (kbd "C-z")  'helm-select-action) ; list actions using C-z
