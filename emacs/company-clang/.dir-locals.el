eed1497215f8a22842149aa792792eb54f56fb84
xenodium <alvaro@xenodium.com>
2014-10-21 22:23:04 +0100

    Adds company-clang for Objective-C.

---
+++ emacs/company-clang/.dir-locals.el
@@ -0,0 +1,8 @@
+((nil . ((company-clang-arguments . ("-include"
+                                     "Foundation/Foundation.h"
+                                     "-include"
+                                     "UIKit/UIKit.h"
+                                     "-isysroot"
+                                     "/Applications/Xcode.app/Contents/Developer/Platforms/iPhoneSimulator.platform/Developer/SDKs/iPhoneSimulator7.1.sdk"
+                                     "-I/Applications/Xcode.app/Contents/Developer/Platforms/iPhoneSimulator.platform/Developer/SDKs/iPhoneSimulator7.1.sdk/System/Library/Frameworks/Foundation.framework/Headers"
+                                     "-I/Applications/Xcode.app/Contents/Developer/Platforms/iPhoneSimulator.platform/Developer/SDKs/iPhoneSimulator7.1.sdk/System/Library/Frameworks/UIKit.framework/Headers")))))
