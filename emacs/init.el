80f2e5bda7544ea7dea4f7b40437d3326663034f
xenodium <alvaro@xenodium.com>
2014-12-19 21:29:57 +0000

    Cleaning up init.el.

--- emacs/init.el
+++ emacs/init.el
@@ -458,7 +458,10 @@
         (message "Buffer '%s' is not visiting a file!" name)
       (if (get-buffer new-name)
           (message "A buffer named '%s' already exists!" new-name)
-        (progn  (rename-file name new-name 1)  (rename-buffer new-name)  (set-visited-file-name new-name)  (set-buffer-modified-p nil)))))) ;;
+        (progn  (rename-file name new-name 1)
+                (rename-buffer new-name)
+                (set-visited-file-name new-name)
+                (set-buffer-modified-p nil))))))
 
 ;; Move buffer file.
 ;; From: https://sites.google.com/site/steveyegge2/my-dot-emacs-file
@@ -466,14 +469,17 @@
   "Moves both current buffer and file it's visiting to DIR." (interactive "DNew directory: ")
   (let* ((name (buffer-name))
          (filename (buffer-file-name))
-         (dir
-          (if (string-match dir "\\(?:/\\|\\\\)$")
-              (substring dir 0 -1) dir))
+         (dir (if (string-match dir "\\(?:/\\|\\\\)$")
+                  (substring dir 0 -1)
+                dir))
          (newname (concat dir "/" name)))
 
     (if (not filename)
         (message "Buffer '%s' is not visiting a file!" name)
-      (progn (copy-file filename newname 1) (delete-file filename) (set-visited-file-name newname) (set-buffer-modified-p nil) t))))
+      (progn (copy-file filename newname 1)
+             (delete-file filename)
+             (set-visited-file-name newname)
+             (set-buffer-modified-p nil) t))))
 
 ; http://scottmcpeak.com/elisp/scott.emacs.el
 ; ------------------- yes-or-no-p ---------------------
