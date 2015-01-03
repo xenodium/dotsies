508c9716b013fed0eb8c61c3cae4fccf590942a2
xenodium <alvaro@xenodium.com>
2015-01-03 15:03:42 +0000

    Adds binding for eval-region and cleans up auto indent yanked.

--- emacs/init.el
+++ emacs/init.el
@@ -1258,12 +1258,21 @@ Repeated invocations toggle between the two most recently open buffers."
 (dolist (command '(yank yank-pop))
   (eval `(defadvice ,command (after indent-region activate)
            (and (not current-prefix-arg)
-                (member major-mode '(emacs-lisp-mode lisp-mode       markdown-mode
-                                                     clojure-mode    scheme-mode
-                                                     haskell-mode    ruby-mode
-                                                     rspec-mode      python-mode
-                                                     c-mode          c++-mode
-                                                     objc-mode       latex-mode
-                                                     plain-tex-mode))
+                (member major-mode '(emacs-lisp-mode
+                                     lisp-mode
+                                     markdown-mode
+                                     clojure-mode
+                                     scheme-mode
+                                     haskell-mode
+                                     ruby-mode
+                                     rspec-mode
+                                     python-mode
+                                     c-mode
+                                     c++-mode
+                                     objc-mode
+                                     latex-mode
+                                     plain-tex-mode))
                 (let ((mark-even-if-inactive transient-mark-mode))
                   (indent-region (region-beginning) (region-end) nil))))))
+
+(global-set-key (kbd "C-x C-r") 'eval-region)
