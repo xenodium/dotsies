1344f0ff53180f9ed175dc8f52819ee8c26944e8
alvaro <alvaro@xenodium.com>
2015-01-06 22:28:22 +0000

    Removing commented out code.

--- emacs/init.el
+++ emacs/init.el
@@ -2,7 +2,6 @@
 (require 'package)
 (add-to-list 'package-archives
              '("melpa" . "http://melpa.org/packages/"))
-(setq package-enable-at-startup nil)
 (package-initialize)
 
 (unless (package-installed-p 'use-package)
