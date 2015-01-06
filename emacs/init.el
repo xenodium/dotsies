3d267a49c5755b30c2c2156b54d95c1a491fcdba
xenodium <alvaro@xenodium.com>
2015-01-06 20:30:34 +0000

    ar/delete-current-file-and-buffer ar/rename-current-file-and-buffer now vc-aware.

--- emacs/init.el
+++ emacs/init.el
@@ -471,21 +471,6 @@
 ;; Case-sensitive fold search search (ie. M-/ to autocomplete).
 (setq dabbrev-case-fold-search nil)
 
-;; Rename file and buffer.
-;; From: https://sites.google.com/site/steveyegge2/my-dot-emacs-file
-(defun ar/rename-file-and-buffer (new-name)
-  "Renames both current buffer and file it's visiting to NEW-NAME." (interactive "sNew name: ")
-  (let ((name (buffer-name))
-        (filename (buffer-file-name)))
-    (if (not filename)
-        (message "Buffer '%s' is not visiting a file!" name)
-      (if (get-buffer new-name)
-          (message "A buffer named '%s' already exists!" new-name)
-        (progn  (rename-file name new-name 1)
-                (rename-buffer new-name)
-                (set-visited-file-name new-name)
-                (set-buffer-modified-p nil))))))
-
 ;; Move buffer file.
 ;; From: https://sites.google.com/site/steveyegge2/my-dot-emacs-file
 (defun ar/move-buffer-file (dir)
@@ -942,18 +927,32 @@ Repeated invocations toggle between the two most recently open buffers."
         (delq (current-buffer)
               (remove-if-not 'buffer-file-name (buffer-list)))))
 
-(defun ar/delete-this-buffer-and-file ()
-  "Removes file connected to current buffer and kills buffer."
+;; From: http://emacsredux.com/blog/2013/05/04/rename-file-and-buffer
+(defun ar/rename-current-file-and-buffer ()
+  "Rename the current buffer and file it is visiting."
   (interactive)
-  (let ((filename (buffer-file-name))
-        (buffer (current-buffer))
-        (name (buffer-name)))
+  (let ((filename (buffer-file-name)))
     (if (not (and filename (file-exists-p filename)))
-        (error "Buffer '%s' is not visiting a file!" name)
-      (when (yes-or-no-p "Are you sure you want to remove this file? ")
-        (delete-file filename)
-        (kill-buffer buffer)
-        (message "File '%s' successfully removed" filename)))))
+        (message "Buffer is not visiting a file!")
+      (let ((new-name (read-file-name "New name: " filename)))
+        (cond
+         ((vc-backend filename) (vc-rename-file filename new-name))
+         (t
+          (rename-file filename new-name t)
+          (set-visited-file-name new-name t t)))))))
+
+;;  From http://emacsredux.com/blog/2013/04/03/delete-file-and-buffer/
+(defun ar/delete-current-file-and-buffer ()
+  "Kill the current buffer and deletes the file it is visiting."
+  (interactive)
+  (let ((filename (buffer-file-name)))
+    (when filename
+      (if (vc-backend filename)
+          (vc-delete-file filename)
+        (progn
+          (delete-file filename)
+          (message "Deleted file %s" filename)
+          (kill-buffer))))))
 
 (use-package go-mode
   :ensure go-mode)
