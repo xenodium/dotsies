4cc73462423a5755746023015a64480f1638705d
xenodium <alvaro@xenodium.com>
2015-01-24 21:32:33 +0000

    Adds flycheck-pos-tip.

--- emacs/init.el
+++ emacs/init.el
@@ -1380,13 +1380,16 @@ Optional argument ΦDIR-PATH-ONLY-P if copying buffer directory."
 (use-package flycheck
   :ensure t)
 
+(use-package flycheck-pos-tip
+  :ensure t)
+
 ;; Override default flycheck triggers
 (setq flycheck-check-syntax-automatically
       '(save idle-change mode-enabled)
       flycheck-idle-change-delay 0.8)
 
 (setq flycheck-display-errors-function
-      #'flycheck-display-error-messages-unless-error-list)
+      #'flycheck-pos-tip-error-messages)
 
 (add-hook 'after-init-hook #'global-flycheck-mode)
 
