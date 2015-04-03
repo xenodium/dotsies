a0625e9283e02507451e956657cba22c27532313
xenodium <alvaro@xenodium.com>
2015-04-03 16:29:57 +0100

    Renaming shell-pop buffer.

--- emacs/init.el
+++ emacs/init.el
@@ -1393,7 +1393,7 @@ Version 2015-02-07."
 ;; Customize shell-pop.
 (setq shell-pop-term-shell "/bin/bash")
 (setq shell-pop-shell-type '("ansi-term"
-                             "*ansi-term*"
+                             "terminal"
                              (lambda
                                nil (ansi-term shell-pop-term-shell))))
 (setq shell-pop-window-position "full")
