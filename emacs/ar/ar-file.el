798ff1842568a79ca9c0b62536cc30e138fbeb19
alvaro <alvaro@xenodium.com>
2015-06-01 22:21:35 +0100

    Cleans up init.el.

--- emacs/ar/ar-file.el
+++ emacs/ar/ar-file.el
@@ -9,6 +9,57 @@
 (require 'ar-string)
 (require 'simple)
 
+;; From: http://emacsredux.com/blog/2013/05/04/rename-file-and-buffer
+(defun ar/file-rename-current ()
+  "Rename the current buffer and file it is visiting."
+  (interactive)
+  (let ((filename (buffer-file-name)))
+    (if (not (and filename (file-exists-p filename)))
+        (message "Buffer is not visiting a file!")
+      (let ((new-name (read-file-name "New name: " filename)))
+        (cond
+         ((vc-backend filename) (vc-rename-file filename new-name))
+         (t
+          (rename-file filename new-name t)
+          (set-visited-file-name new-name t t)))))))
+
+;;  From http://emacsredux.com/blog/2013/04/03/delete-file-and-buffer/
+(defun ar/file-delete-current-file ()
+  "Kill the current buffer and deletes the file it is visiting."
+  (interactive)
+  (let ((filename (buffer-file-name)))
+    (when filename
+      (if (vc-backend filename)
+          (vc-delete-file filename)
+        (progn
+          (delete-file filename)
+          (message "Deleted file %s" filename)
+          (kill-buffer))))))
+
+;; Move buffer file.
+;; From: https://sites.google.com/site/steveyegge2/my-dot-emacs-file
+(defun ar/file-move (dir)
+  "Move both current buffer and file it's visiting to DIR." (interactive "DNew directory: ")
+  (let* ((name (buffer-name))
+         (filename (buffer-file-name))
+         (dir (if (string-match dir "\\(?:/\\|\\\\)$")
+                  (substring dir 0 -1)
+                dir))
+         (newname (concat dir "/" name)))
+
+    (if (not filename)
+        (message "Buffer '%s' is not visiting a file!" name)
+      (progn (copy-file filename newname 1)
+             (delete-file filename)
+             (set-visited-file-name newname)
+             (set-buffer-modified-p nil) t))))
+
+(defmacro ar/file-with-current-file (file-path &rest body)
+  "Open file at FILE-PATH and execute BODY."
+  `(with-current-buffer (find-file-noselect (expand-file-name ,file-path))
+     (save-excursion
+       (progn ,@body))))
+
 (defun ar/file-read-image-name ()
   "Read image file name."
   (read-file-name "Choose image: " nil nil t nil
