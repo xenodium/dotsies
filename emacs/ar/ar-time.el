526468d4f9b1cd3cace7de52dbed5a2a07b6aec8
xenodium <alvaro@xenodium.com>
2015-06-03 19:12:27 +0100

    Adds helpers to simplify introducing tasks.

---
+++ emacs/ar/ar-time.el
@@ -0,0 +1,24 @@
+;;; ar-time.el --- Time support.
+
+;;; Commentary:
+;; Time helpers.
+
+
+;;; Code:
+
+(defun ar/time-between-p (time floor ceiling)
+  "Return non nil if TIME between FLOOR and CEILING."
+  (and (time-less-p time ceiling) (time-less-p floor time)))
+
+(defun ar/time-increment-by-days (time days)
+  "Increment TIME by number of DAYS."
+  (time-add time (days-to-time days)))
+
+(defun ar/time-weekday-p (time)
+  "Return non nil if TIME is week day."
+  (let ((weekday (nth 6 (decode-time time))))
+    (and (> weekday 0) (< weekday 6))))
+
+(provide 'ar-time)
+
+;;; ar-time.el ends here
