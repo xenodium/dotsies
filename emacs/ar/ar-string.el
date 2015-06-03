209d03868da089b3026fd8f557b7bc736b4ee115
alvaro <alvaro@xenodium.com>
2015-06-03 09:10:19 +0100

    Splits init up (WIP).

--- emacs/ar/ar-string.el
+++ emacs/ar/ar-string.el
@@ -10,6 +10,18 @@
   "Join strings in STRINGS list with spaces."
   (mapconcat 'identity strings " "))
 
+(defun ar/string-match-p (regex string)
+  "Return t if REGEX matches STRING. nil otherwise."
+  (if (string-match regex string) t nil))
+
+(defun ar/string-numeric-p (string)
+  "Return t if STRING is an unsigned integer.  nil otherwise."
+  (ar/string-match-p "\\`[[:digit:]]+\\'" string))
+
+(defun ar/string-alpha-numeric-p (string)
+  "Return t if STRING is alphanumeric.  nil otherwise."
+  (ar/string-match-p "\\`[[:alnum:]]+\\'" string))
+
 (provide 'ar-string)
 
 ;;; ar-string.el ends here
