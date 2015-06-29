f9797c2d9a671ababff4ec8fd6e75dab83c1bc70
xenodium <me+gh@xenodium.com>
2015-06-29 21:53:53 +0100

    Fixes ar-buffer bug and adds org-src-fontify-natively.

--- emacs/ar/ar-buffer.el
+++ emacs/ar/ar-buffer.el
@@ -57,6 +57,7 @@
 
 (defun ar/buffer-string-match-p (re)
   "Return t if RE matches current buffer. nil otherwise."
+  (goto-char 0)
   (re-search-forward re nil t))
 
 (defun ar/buffer-kill-others ()
