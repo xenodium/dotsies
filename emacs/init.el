6552c72eb3636a846423e58f9bc7c7783be061e0
xenodium <me+gh@xenodium.com>
2015-08-20 08:15:38 +0100

    Adds ar/comment-or-uncomment-sexp.

--- emacs/init.el
+++ emacs/init.el
@@ -1229,6 +1229,89 @@ Argument LEN Length."
 
 (add-hook 'term-mode-hook #'ar/term-mode-hook-function)
 
+;; From http://endlessparentheses.com/a-comment-or-uncomment-sexp-command.html
+(defun ar/uncomment-sexp (&optional n)
+  "Uncomment a sexp around point."
+  (interactive "P")
+  (let* ((initial-point (point-marker))
+         (p)
+         (end (save-excursion
+                (when (elt (syntax-ppss) 4)
+                  (re-search-backward comment-start-skip
+                                      (line-beginning-position)
+                                      t))
+                (setq p (point-marker))
+                (comment-forward (point-max))
+                (point-marker)))
+         (beg (save-excursion
+                (forward-line 0)
+                (while (= end (save-excursion
+                                (comment-forward (point-max))
+                                (point)))
+                  (forward-line -1))
+                (goto-char (line-end-position))
+                (re-search-backward comment-start-skip
+                                    (line-beginning-position)
+                                    t)
+                (while (looking-at-p comment-start-skip)
+                  (forward-char -1))
+                (point-marker))))
+    (unless (= beg end)
+      (uncomment-region beg end)
+      (goto-char p)
+      ;; Indentify the "top-level" sexp inside the comment.
+      (while (and (ignore-errors (backward-up-list) t)
+                  (>= (point) beg))
+        (skip-chars-backward (rx (syntax expression-prefix)))
+        (setq p (point-marker)))
+      ;; Re-comment everything before it. 
+      (ignore-errors
+        (comment-region beg p))
+      ;; And everything after it.
+      (goto-char p)
+      (forward-sexp (or n 1))
+      (skip-chars-forward "\r\n[:blank:]")
+      (if (< (point) end)
+          (ignore-errors
+            (comment-region (point) end))
+        ;; If this is a closing delimiter, pull it up.
+        (goto-char end)
+        (skip-chars-forward "\r\n[:blank:]")
+        (when (= 5 (car (syntax-after (point))))
+          (delete-indentation))))
+    ;; Without a prefix, it's more useful to leave point where
+    ;; it was.
+    (unless n
+      (goto-char initial-point))))
+
+(defun ar/comment-sexp--raw ()
+  "Comment the sexp at point or ahead of point."
+  (pcase (or (bounds-of-thing-at-point 'sexp)
+             (save-excursion
+               (skip-chars-forward "\r\n[:blank:]")
+               (bounds-of-thing-at-point 'sexp)))
+    (`(,l . ,r)
+     (goto-char r)
+     (skip-chars-forward "\r\n[:blank:]")
+     (comment-region l r)
+     (skip-chars-forward "\r\n[:blank:]"))))
+
+(defun ar/comment-or-uncomment-sexp (&optional n)
+  "Comment the sexp at point and move past it.
+If already inside (or before) a comment, uncomment instead.
+With a prefix argument N, (un)comment that many sexps."
+  (interactive "P")
+  (if (or (elt (syntax-ppss) 4)
+          (< (save-excursion
+               (skip-chars-forward "\r\n[:blank:]")
+               (point))
+             (save-excursion
+               (comment-forward 1)
+               (point))))
+      (ar/uncomment-sexp n)
+    (dotimes (_ (or n 1))
+      (ar/comment-sexp--raw))))
+
 (defun ar/comment-dwim ()
   "Comment current line or region."
   (interactive)
@@ -1932,7 +2015,7 @@ _y_outube
                         (format-time-string "[%Y-%m-%d %a]")))
 
 (defun ar/org-confirm-babel-evaluate (lang body)
-  "Do not confirm org babel evaluation for known languages."
+  "Do not confirm org babel evaluation for LANG and BODY."
   (and
    (not (string= lang "emacs-lisp"))
    (not (string= lang "plantuml"))))
@@ -1946,7 +2029,7 @@ _y_outube
            jar-path-osx)
           ((file-exists-p jar-path-linux)
            jar-path-linux)
-          ((error "Error: plantuml not installed on platform.")))))
+          ((error "Error: plantuml not installed on platform")))))
 
 (use-package ob-plantuml
   :commands org-babel-execute:plantuml
