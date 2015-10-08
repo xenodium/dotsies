f09154b1cc3348c48781d6e31f774eac5b1c1987
xenodium <me+gh@xenodium.com>
2015-10-08 20:23:46 +0100

    Adds ar-shell.el.

--- emacs/ar/ar-helm-projectile.el
+++ emacs/ar/ar-helm-projectile.el
@@ -5,20 +5,10 @@
 
 ;;; Code:
 
+(require 'ar-shell)
 (require 'helm-projectile)
 
-(defun ar/shell-cd (dir-path)
-  "Change shell current working directory to DIR-PATH."
-  (unless (string-equal mode-name "Shell")
-    (error "Not in Shell mode"))
-  (message mode-name)
-  (goto-char (point-max))
-  (comint-kill-input)
-  (insert (concat "cd " (shell-quote-argument dir-path)))
-  (let ((comint-process-echoes t))
-    (comint-send-input)))
-
-(defun ar/helm-shell-projectile-cd ()
+(defun ar/helm-projectile-shell-cd ()
   "Change shell shell current working directory using helm projectile."
   (interactive)
   (unless (string-equal mode-name "Shell")
