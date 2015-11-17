7e65ef8cd6b2d3cd07f437f348f30a74ef23590c
xenodium <alvaro@xenodium.com>
2015-11-17 19:53:17 +0000

    Make package sources more secure.

---
+++ emacs/ar/ar-package.el
@@ -0,0 +1,34 @@
+;;; ar-package.el --- Package support
+
+;;; Commentary:
+;; Package helpers.
+
+
+;;; Code:
+
+(require 'package)
+(require 'ar-process)
+(require 'ar-python)
+
+(defun ar/package-initialize ()
+  "Initialize package sources more securely.
+Based on: https://glyph.twistedmatrix.com/2015/11/editor-malware.html"
+  (setq package-archives `(("gnu" . "https://elpa.gnu.org/packages/")
+                           ("melpa" . "https://melpa.org/packages/")))
+  (ar/process-assert-binary-installed "gnutls-cli")
+  (let ((trustfile
+         (replace-regexp-in-string
+          "\\\\" "/"
+          (replace-regexp-in-string
+           "\n" ""
+           (ar/python-run-module-as-script "certifi")))))
+    (setq tls-program
+          (list
+           (format "gnutls-cli --x509cafile %s -p %%p %%h" trustfile)))
+    (setq gnutls-trustfiles (list trustfile)))
+  (setq tls-checktrust t)
+  (package-initialize))
+
+(provide 'ar-package)
+
+;;; ar-package.el ends here
