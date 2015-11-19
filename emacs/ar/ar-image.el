9a573fa8f70b083694e6948f21dfdd6f5dae06e1
xenodium <alvaro@xenodium.com>
2015-11-19 16:35:14 +0000

    Makes ar/image-open-html-for-dir-path non-recursive.

--- emacs/ar/ar-image.el
+++ emacs/ar/ar-image.el
@@ -4,7 +4,6 @@
 ;; Image helpers.
 
 (require 'ar-buffer)
-(require 'ar-file)
 (require 'ar-string)
 
 ;;; Code:
@@ -12,14 +11,14 @@
 (defun ar/image-open-html-for-current-dir ()
   "Open generated HTML for current dir path."
   (interactive)
-  (ar/image-open-html-for-dir-path (ar/buffer-current-dir-path) 15 "jpg"))
+  (ar/image-open-html-for-dir-path (ar/buffer-current-dir-path) 15))
 
-(defun ar/image-open-html-for-dir-path (dir-path image-percentage-size image-extension)
+(defun ar/image-open-html-for-dir-path (dir-path image-percentage-size)
   "Open generated HTML page for DIR-PATH, IMAGE-PERCENTAGE-SIZE, and IMAGE-EXTENSION.  For example: (ar/image-open-html-for-dir-path \"path/to/images/\" 10 \"jpg\")."
   (let ((body-html-template "<html><header><title>{{dir-path}}</title></header><body><h1>{{dir-path}}<h1/>{{images}}</body></html>")
         (image-html-template (format "<img width='%d%%' src='{{image-path}}'/>" image-percentage-size))
         (images-html "")
-        (images-paths (ar/file-find (format "*\\\\.%s" image-extension) (expand-file-name dir-path)))
+        (images-paths (directory-files (expand-file-name dir-path) t "\\(png\\|jpg\\|bmp\\|gif\\)$"))
         (output-file-path (format "/tmp/%d.html" (random 9999))))
     (assert (> (length images-paths) 0) nil "No images found")
     (mapc (lambda (image-path)
