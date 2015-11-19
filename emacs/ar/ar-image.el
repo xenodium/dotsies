0b6f2090887a5b63b4551f20a1228be725e21203
xenodium <me+gh@xenodium.com>
2015-11-19 21:49:14 +0000

    Handle base64 in ar/image-open-html-for-current-dir.

--- emacs/ar/ar-image.el
+++ emacs/ar/ar-image.el
@@ -8,12 +8,12 @@
 
 ;;; Code:
 
-(defun ar/image-open-html-for-current-dir ()
-  "Open generated HTML for current dir path."
-  (interactive)
-  (ar/image-open-html-for-dir-path (ar/buffer-current-dir-path) 15))
+(defun ar/image-open-html-for-current-dir (use-base-64)
+  "Open generated HTML for current dir path. With prefix, use base64 encoded images."
+  (interactive "P")
+  (ar/image-open-html-for-dir-path (ar/buffer-current-dir-path) 15 use-base-64))
 
-(defun ar/image-open-html-for-dir-path (dir-path image-percentage-size)
+(defun ar/image-open-html-for-dir-path (dir-path image-percentage-size &optional use-base-64)
   "Open generated HTML page for DIR-PATH, IMAGE-PERCENTAGE-SIZE, and IMAGE-EXTENSION.  For example: (ar/image-open-html-for-dir-path \"path/to/images/\" 10 \"jpg\")."
   (let ((body-html-template "<html><header><title>{{dir-path}}</title></header><body><h1>{{dir-path}}<h1/>{{images}}</body></html>")
         (image-html-template (format "<img width='%d%%' src='{{image-path}}'/>" image-percentage-size))
@@ -22,6 +22,10 @@
         (output-file-path (format "/tmp/%d.html" (random 9999))))
     (assert (> (length images-paths) 0) nil "No images found")
     (mapc (lambda (image-path)
+            (when use-base-64
+              (setq image-path (format "data:image/%s;base64,%s"
+                                       (file-name-extension image-path)
+                                       (shell-command-to-string (format "base64 -i \"%s\"" image-path)))))
             (setq images-html
                   (concat images-html
                           (replace-regexp-in-string "{{image-path}}"
