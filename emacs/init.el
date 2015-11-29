1e85573239b9e901a745f6763dfec9a75e06d4f4
xenodium <me+gh@xenodium.com>
2015-11-29 19:17:19 +0000

    Make kill-region and kill-ring-save operate on lines without region.

--- emacs/init.el
+++ emacs/init.el
@@ -2212,6 +2212,25 @@ _y_outube
 (setq sunshine-units 'metric)
 (setq sunshine-location "London, GB")
 
+;; From https://github.com/daschwa/emacs.d
+(defadvice kill-region (before slick-cut activate compile)
+  "When called interactively with no active region, kill a single
+line instead."
+  (interactive
+   (if mark-active
+       (list (region-beginning) (region-end))
+     (list (line-beginning-position) (line-beginning-position 2)))))
+
+;; From https://github.com/daschwa/emacs.d
+(defadvice kill-ring-save (before slick-copy activate compile)
+  "When called interactively with no active region, copy a single
+line instead."
+  (interactive
+   (if mark-active
+       (list (region-beginning) (region-end))
+     (message "Copied line")
+     (list (line-beginning-position) (line-beginning-position 2)))))
+
 ;; From http://www.reddit.com/r/emacs/comments/2amn1v/isearch_selected_text
 (defadvice isearch-mode (around isearch-mode-default-string (forward &optional regexp op-fun recursive-edit word-p) activate)
   "Enable isearch to start with current selection."
