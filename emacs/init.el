80ad451344f47da926aa7304cb6d2d98862cbe57
xenodium <alvaro@xenodium.com>
2015-12-01 00:06:17 +0000

    Adds speed-type.

--- emacs/init.el
+++ emacs/init.el
@@ -125,23 +125,24 @@
   "Set up graphical mode line."
   (use-package spaceline :ensure t
     :config
-    (require 'spaceline-config)
-    (spaceline-toggle-minor-modes-off)
-    (spaceline-toggle-buffer-encoding-off)
-    (spaceline-toggle-buffer-encoding-abbrev-off)
-    (setq powerline-default-separator 'arrow)
-    (setq spaceline-highlight-face-func 'spaceline-highlight-face-evil-state)
-    (spaceline-define-segment line-column
-      "The current line and column numbers."
-      "l:%l c:%2c")
-    (spaceline-define-segment time
-      "The current time."
-      (format-time-string "%H:%M"))
-    (spaceline-define-segment date
-      "The current date."
-      (format-time-string "%h %d"))
-    (spaceline-toggle-time-on)
-    (spaceline-emacs-theme 'date 'time)))
+    (use-package spaceline-config
+      :config
+      (spaceline-toggle-minor-modes-off)
+      (spaceline-toggle-buffer-encoding-off)
+      (spaceline-toggle-buffer-encoding-abbrev-off)
+      (setq powerline-default-separator 'rounded)
+      (setq spaceline-highlight-face-func 'spaceline-highlight-face-evil-state)
+      (spaceline-define-segment line-column
+        "The current line and column numbers."
+        "l:%l c:%2c")
+      (spaceline-define-segment time
+        "The current time."
+        (format-time-string "%H:%M"))
+      (spaceline-define-segment date
+        "The current date."
+        (format-time-string "%h %d"))
+      (spaceline-toggle-time-on)
+      (spaceline-emacs-theme 'date 'time))))
 
 ;; TODO: Revisit this.
 (defun ar/setup-graphical-display ()
@@ -159,6 +160,8 @@
   :commands (totd)
   :config
   (totd-start))
+
+(use-package speed-type :ensure t)
 
 (use-package restclient :ensure t
   :commands (restclient-mode))
