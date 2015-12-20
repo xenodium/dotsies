63247185daa8666818ed9e5d5574860ecfde77f3
xenodium <alvaro@xenodium.com>
2015-12-20 23:32:33 +0000

    Use assert instead of unless and error.

--- emacs/ar/ar-helm-projectile.el
+++ emacs/ar/ar-helm-projectile.el
@@ -6,13 +6,13 @@
 ;;; Code:
 
 (require 'ar-shell)
+(require 'cl)
 (require 'helm-projectile)
 
 (defun ar/helm-projectile-shell-cd ()
   "Change shell current working directory using helm projectile."
   (interactive)
-  (unless (string-equal mode-name "Shell")
-    (error "Not in Shell mode"))
+  (assert (string-equal mode-name "Shell") nil "Not in Shell mode")
   (let ((helm-dir-source (copy-tree  helm-source-projectile-directories-list)))
     (add-to-list 'helm-dir-source '(action . ar/shell-cd))
     (add-to-list 'helm-dir-source '(keymap . nil))
