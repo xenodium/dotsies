1475d908b3a9e00288ffc423aa6d5c5f87bac3cd
xenodium <alvaro@xenodium.com>
2015-12-30 00:19:44 +0000

    Split dired window between Downloads and active dir

---
+++ emacs/ar/ar-dired.el
@@ -0,0 +1,30 @@
+;;; ar-dired.el --- Dired support.
+
+;;; Commentary:
+;; Dired helpers.
+
+
+;;; Code:
+
+(defun ar/dired-split-directories (dir-a dir-b)
+  (delete-other-windows)
+  (split-window-right)
+  (find-file dir-a)
+  (find-file-other-window dir-b)
+  (other-window 1))
+
+(defun ar/dired-split-downloads-to-active ()
+  "Split window with downloads to active directory."
+  (interactive)
+  (ar/dired-split-directories "~/Downloads"
+                              "~/stuff/active"))
+
+(defun ar/dired-split-desktop-to-active ()
+  "Split window with downloads to active directory."
+  (interactive)
+  (ar/dired-split-directories "~/Desktop"
+                              "~/stuff/active"))
+
+(provide 'ar-dired)
+
+;;; ar-dired.el ends here
