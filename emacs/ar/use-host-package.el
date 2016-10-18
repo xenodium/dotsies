1c76f81f1dfc15710dfcfa2ab76d3056dc747d69
xenodium <me+gh@xenodium.com>
2016-10-18 09:44:16 +0100

    Cleaning up use-host-package and use it for figlet

--- emacs/ar/use-host-package.el
+++ emacs/ar/use-host-package.el
@@ -3,10 +3,6 @@
 ;;; Commentary:
 ;; Like use-package but for host dependencies.
 
-;; (use-host-package
-;;  :bin "figlet"
-;;  :name "figlet")
-
 ;;; Code:
 
 (require 'files)
@@ -15,17 +11,35 @@
 (defvar use-host-package-install-cmd nil "Install command (ie. \"brew install\" or \"apt-get install\")")
 
 (defun use-host-package (&rest plist-args)
-  (unless (apply #'use-host-package--installed-p plist-args)
-    (apply #'use-host-package--install plist-args)
-    (assert (apply #'use-host-package--installed-p plist-args)
-            nil "Unfulfilled check after installation")))
+  (unless (use-host-package--installed-p (plist-get plist-args :name)
+                                         (plist-get plist-args :bin))
+    (use-host-package--install (plist-get plist-args :name)
+                               (plist-get plist-args :install-name)
+                               (plist-get plist-args :install-command))
+    (assert (use-host-package--installed-p (plist-get plist-args :name)
+                                           (plist-get plist-args :bin))
+            nil "Unfulfilled check after installation"))
+  (plist-get plist-args :name))
 
-(defun use-host-package--assert-variables ()
-  (assert use-host-package-install-cmd nil "use-host-package-install-cmd must be set"))
-
-(defun use-host-package--install (&rest plist-args)
-  (assert use-host-package-install-cmd nil "use-host-package-install-cmd must be set")
-  (let ((host-package-name (plist-get plist-args :name)))
+(defun use-host-package--install (host-package-name
+                                  install-name-override
+                                  install-command-override)
+  (let ((install-command (cond (install-command-override
+                                install-command-override)
+                               (install-name-override
+                                (assert use-host-package-install-cmd
+                                        nil
+                                        "use-host-package-install-cmd must be set")
+                                (format "%s %s"
+                                        use-host-package-install-cmd
+                                        (shell-quote-argument install-name-override)))
+                               (host-package-name
+                                (assert use-host-package-install-cmd
+                                        nil
+                                        "use-host-package-install-cmd must be set")
+                                (format "%s %s"
+                                        use-host-package-install-cmd
+                                        (shell-quote-argument host-package-name))))))
     (message "Installing %s..." host-package-name)
     (assert (= 0 (call-process-shell-command (format "%s %s"
                                                      use-host-package-install-cmd
@@ -34,14 +48,11 @@
             nil (format "Unable to install %s" host-package-name))
     (message "Installed %s" host-package-name)))
 
-(defun use-host-package--installed-p (&rest plist-args)
-  (cond ((apply #'use-host-package--bin-installed-p plist-args))))
-
-(defun use-host-package--bin-installed-p (&rest plist-args)
-  (let ((bin-name (or (plist-get plist-args :bin)
-                      (plist-get plist-args :name))))
-    (when bin-name
-      (executable-find bin-name))))
+(defun use-host-package--installed-p (host-package-name &optional bin-name)
+  (cond (bin-name
+         (executable-find bin-name))
+        (host-package-name
+         (executable-find host-package-name))))
 
 (defun use-host-package--install-buffer ()
   (let ((install-buffer-name "*use-host-package install*"))
@@ -49,19 +60,13 @@
       (erase-buffer)
       (get-buffer install-buffer-name))))
 
-(ert-deftest use-host-package--bin-installed-p-test ()
-  (should (use-host-package--bin-installed-p :bin "ls"))
-  (should-not (use-host-package--bin-installed-p :bin "bin-not-found"))
-  (should (use-host-package--bin-installed-p :name "ls"))
-  (should-not (use-host-package--bin-installed-p :name "bin-not-found"))
-  (should-not (use-host-package--bin-installed-p :other-args "other-values")))
-
 (ert-deftest use-host-package--installed-p-test ()
-  (should (use-host-package--installed-p :bin "ls"))
-  (should-not (use-host-package--installed-p :bin "bin-not-found"))
-  (should (use-host-package--installed-p :name "ls"))
-  (should-not (use-host-package--installed-p :name "bin-not-found"))
-  (should-not (use-host-package--installed-p :other-args "other-values")))
+  (should (use-host-package--installed-p "ls"))
+  (should-not (use-host-package--installed-p "bin-not-found"))
+  (should (use-host-package--installed-p "bin-not-found"
+                                         "ls"))
+  (should-not (use-host-package--installed-p "bin-not-found"
+                                             "bin-not-found")))
 
 (provide 'use-host-package)
 
