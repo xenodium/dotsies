b485b4b2caf9d529834dda29c2ee4082caa2cfe3
xenodium <me+gh@xenodium.com>
2017-08-18 08:32:40 +0100

    Tweaking company-bash-history

--- emacs/ar/company-bash-history.el
+++ emacs/ar/company-bash-history.el
@@ -21,9 +21,17 @@
 
   (case command
     (interactive (company-begin-backend 'company-bash-history))
-    (prefix (company-grab-symbol))
+    (prefix
+     (company-grab-symbol-cons (regexp-quote "$ ") 2))
     (candidates
-     (company-bash-history-value (substring-no-properties (company-grab-symbol) 0 (length (company-grab-symbol)))))))
+     (company-bash-candidates (company-grab-symbol-cons (regexp-quote "$ ") 2)))))
+
+(defun company-bash-candidates (value)
+  (if (consp value)
+      (let ((search-term (car value))
+            (trigger-found (cdr value)))
+        (when trigger-found
+          (company-bash-history-value (substring-no-properties search-term 0 (length search-term)))))))
 
 (provide 'company-bash-history)
 
