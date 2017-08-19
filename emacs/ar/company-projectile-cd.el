a990180ff5839342890038cac904c9af6d09b9ba
xenodium <alvaro@xenodium.com>
2017-08-20 00:50:38 +0100

    Tweaking company-projectile-cd

--- emacs/ar/company-projectile-cd.el
+++ emacs/ar/company-projectile-cd.el
@@ -8,9 +8,11 @@
 
 (require 'cl-lib)
 (require 'company)
+(require 'dash)
+(require 'f)
 (require 'projectile)
 
-(defvar company-projectile-cd-prefix "cd ")
+(defvar-local company-projectile-cd-prefix "cd ")
 
 (defun company-projectile-cd (command &optional arg &rest ignored)
   "Company shell completion for any projectile path."
@@ -21,32 +23,40 @@
      (company-grab-symbol-cons company-projectile-cd-prefix
                                (length company-projectile-cd-prefix)))
     (candidates
-     (company-projectile-cd--candidates (company-grab-symbol-cons company-projectile-cd-prefix
-                                                                  (length company-projectile-cd-prefix))))
+     (company-projectile-cd--candidates
+      (company-grab-symbol-cons company-projectile-cd-prefix
+                                (length company-projectile-cd-prefix))))
     (post-completion
      (company-projectile-cd--expand-inserted-path arg))))
 
-(defun company-projectile--filter (paths needle)
-  "Keep PATHS matching NEEDLE."
-  (delq nil
-        (mapcar (lambda (path)
-                  (when (string-match-p needle path)
-                    path))
-                paths)))
-
 (defun company-projectile-cd--candidates (input)
   "Return candidates for given INPUT."
-  (if (consp input)
-      (let ((search-term (car input))
-            (trigger-found (cdr input)))
-        (when trigger-found
-          (company-projectile--filter (projectile-current-project-dirs)
-                                      (substring-no-properties search-term 0 (length search-term)))))))
+  (company-projectile-cd--reset-root)
+  (when (and (projectile-project-p) (consp input))
+    (let ((search-term (substring-no-properties
+                        (car input) 0 (length (car input))))
+          (prefix-found (cdr input)))
+      (when prefix-found
+        (-filter (lambda (path)
+                   (string-match-p (regexp-quote
+                                    search-term)
+                                   path))
+                 (-snoc
+                  (projectile-current-project-dirs)
+                  ;; Throw project root in there also.
+                  (projectile-project-root)))))))
 
 (defun company-projectile-cd--expand-inserted-path (path)
-  "Delete relative PATH insertion with its absolute equivalent."
-  (delete-region (point) (- (point) (length path)))
-  (insert (concat (projectile-project-root) path)))
+  "Replace relative PATH insertion with its absolute equivalent if needed."
+  (unless (f-exists-p path)
+    (delete-region (point) (- (point) (length path)))
+    (insert (concat (projectile-project-root) path))))
+
+(defun company-projectile-cd--reset-root ()
+  "Reset project root. Useful when cd'ing in and out of projects."
+  (projectile-reset-cached-project-root)
+  (when (projectile-project-p)
+    (projectile-project-root)))
 
 (provide 'company-projectile-cd)
 
