e9d9cc5f8dd2abedff6a1b2d8e2adb4702e1da73
xenodium <me+gh@xenodium.com>
2017-11-04 20:43:42 +0000

    Use major-mode when checking for shell (instead of mode-name)

--- emacs/ar/ar-helm-projectile.el
+++ emacs/ar/ar-helm-projectile.el
@@ -13,14 +13,14 @@
 (defun ar/helm-projectile-shell-cd ()
   "Change shell current working directory using helm projectile."
   (interactive)
-  (assert (or (string-equal mode-name "EShell")
-              (string-equal mode-name "Shell")) nil "Not in (E)Shell mode")
+  (assert (or (equal major-mode 'eshell-mode)
+              (equal major-mode 'shell-mode)) nil "Not in (E)Shell mode")
   (let ((helm-dir-source (copy-tree  helm-source-projectile-directories-list)))
     (add-to-list 'helm-dir-source (cons 'action (if (string-equal mode-name "EShell")
                                                     (lambda (path)
                                                       (insert (format "cd %s" path))
                                                       (eshell-send-input))
-                                                    'ar/shell-cd)))
+                                                  'ar/shell-cd)))
     (add-to-list 'helm-dir-source '(keymap . nil))
     (add-to-list 'helm-dir-source '(header-line . "cd to directory..."))
     (helm :sources helm-dir-source
