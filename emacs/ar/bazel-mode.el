47def72dee2c9b871ed01f96bfa03ab1b3b40fa4
xenodium <me+gh@xenodium.com>
2017-11-30 08:51:08 +0000

    Simpifying bazel-mode

--- emacs/ar/bazel-mode.el
+++ emacs/ar/bazel-mode.el
@@ -6,23 +6,16 @@
 
 ;;; Code:
 
-(require 'python)
+(require 'prog-mode)
 
-(define-derived-mode bazel-mode python-mode "Bazel")
+(define-derived-mode bazel-mode prog-mode "Bazel")
 
-(defun ar/bazel-mode--imenu-index-fun ()
-  "Implemented, since `python-mode' set `imenu-create-index-function'."
-  (let ((mode-imenu (imenu-default-create-index-function))
-        (custom-imenu (imenu--generic-function imenu-generic-expression)))
-    (append mode-imenu custom-imenu)))
-
-(defun ar/bazel-mode-hook-fun ()
+(defun bazel-mode--hook-fun ()
   "Configure mode in hook."
-  (setq-local imenu-create-index-function #'ar/bazel-mode--imenu-index-fun)
   (setq-local imenu-generic-expression
               '(("Build rule" "name *= *\"\\(.*\\)\"" 1))))
 
-(add-hook 'bazel-mode-hook #'ar/bazel-mode-hook-fun)
+(add-hook 'bazel-mode-hook #'bazel-mode--hook-fun)
 
 (add-to-list 'auto-mode-alist '("BUILD\\'" . bazel-mode))
 
