da8549d2c9d2b8c2860f9efe8b2c4dec01158686
xenodium <alvaro@xenodium.com>
2017-12-13 23:47:28 +0000

    Adding some dired helpers

--- emacs/ar/ar-dired.el
+++ emacs/ar/ar-dired.el
@@ -6,6 +6,10 @@
 
 ;;; Code:
 
+(require 'f)
+
+(require 'ar-platform)
+
 (defun ar/dired-split-directories (dir-a dir-b)
   (delete-other-windows)
   (split-window-right)
@@ -13,23 +17,94 @@
   (find-file-other-window dir-b)
   (other-window 1))
 
-(defun ar/dired-split-downloads-to-active ()
+(defun ar/dired-current-directory ()
+  "Get the current directory from buffer, including `dired-mode'."
+  (if(eq major-mode 'dired-mode)
+      (buffer-file-name)
+    (file-name-directory (buffer-file-name))))
+
+(defun ar/dired-split-downloads-to-current ()
   "Split window with downloads to active directory."
   (interactive)
   (ar/dired-split-directories "~/Downloads"
-                              "~/stuff/active"))
+                              (ar/dired-current-directory)))
 
-(defun ar/dired-split-desktop-to-active ()
+(defun ar/dired-split-desktop-to-current ()
   "Split window with downloads to active directory."
   (interactive)
   (ar/dired-split-directories "~/Desktop"
-                              "~/stuff/active"))
+                              (ar/dired-current-directory)))
 
-(defun ar/dired-split-active-to-active ()
+(defun ar/dired-split-active-to-current ()
   "Split window with active to active directory."
   (interactive)
   (ar/dired-split-directories "~/stuff/active"
-                              "~/stuff/active"))
+                              (ar/dired-current-directory)))
+
+(defun ar/dired--choose-from (path)
+  "Choose a file from PATH."
+  (completing-read "Choose: " (f-files path)))
+
+(defun ar/dired--copy-or-move (fun path)
+  "Copy/move using FUN to file PATH."
+  (let* ((from-path (ar/dired--choose-from (f-expand path)))
+         (to-path (f-join (ar/dired-current-directory)
+                          (f-filename from-path))))
+    (funcall fun from-path to-path)
+    (when (not (eq major-mode 'dired-mode))
+      (find-file (ar/dired-current-directory)))
+    (dired-goto-file to-path)))
+
+(defun ar/dired--find-file-from (path)
+  "Find file choosing from PATH."
+  (let ((file-path (ar/dired--choose-from (f-expand path))))
+    (find-file file-path)))
+
+(defun ar/dired--open-file-externally-from (path)
+  "Open file externally choosing from PATH."
+  (let ((file-path (ar/dired--choose-from (f-expand path))))
+    (funcall (ar/platform-open-in-external-app-function) file-path)))
+
+(defun ar/dired-open-file-externally-from-downloads ()
+  "Open a file externally from Downloads directory."
+  (interactive)
+  (ar/dired--open-file-externally-from "~/Downloads"))
+
+(defun ar/dired-find-file-from-downloads ()
+  "Move a file from Downloads directory to current."
+  (interactive)
+  (ar/dired--find-file-from "~/Downloads"))
+
+(defun ar/dired-move-file-from-downloads ()
+  "Move a file from Downloads directory to current."
+  (interactive)
+  (ar/dired--copy-or-move 'f-move "~/Downloads"))
+
+(defun ar/dired-copy-file-from-downloads ()
+  "Copy a file from Downloads directory to current."
+  (interactive)
+  (ar/dired--copy-or-move 'f-copy "~/Downloads"))
+
+
+(defun ar/dired-open-file-externally-from-desktop ()
+  "Open a file externally from Desktop directory."
+  (interactive)
+  (ar/dired--open-file-externally-from "~/Desktop"))
+
+(defun ar/dired-find-file-from-desktop ()
+  "Move a file from Desktop directory to current."
+  (interactive)
+  (ar/dired--find-file-from "~/Desktop"))
+
+(defun ar/dired-move-file-from-desktop ()
+  "Move a file from Desktop directory to current."
+  (interactive)
+  (ar/dired--copy-or-move 'f-move "~/Desktop"))
+
+(defun ar/dired-copy-file-from-desktop ()
+  "Copy a file from Desktop directory to current."
+  (interactive)
+  (ar/dired--copy-or-move 'f-copy "~/Desktop"))
 
 (defun ar/dired-get-size ()
   "Show total for selected files."
