714ecfd3f8aa1e5092ff6a3fe82826abff5d9386
xenodium <me+gh@xenodium.com>
2018-04-05 17:55:47 +0100

    Adding emojis to hammerspoon

--- mac/hammerspoon/init.lua
+++ mac/hammerspoon/init.lua
@@ -49,3 +49,34 @@ end
 hs.hotkey.bind({"alt"}, "T", addEmacsOrgModeTODO)
 hs.hotkey.bind({"alt"}, "E", function() activateApp("org.gnu.Emacs", "Emacs") end)
 hs.hotkey.bind({"alt"}, "X", function() activateApp("com.apple.dt.Xcode", "Xcode") end)
+
+local choices = {}
+for _, emoji in ipairs(hs.json.decode(io.open("emojis/emojis.json"):read())) do
+   table.insert(choices,
+                {text=emoji['name'],
+                 subText=table.concat(emoji['kwds'], ", "),
+                 image=hs.image.imageFromPath("emojis/" .. emoji['id'] .. ".png"),
+                 chars=emoji['chars']
+   })
+end
+
+-- Focus the last used window.
+local function focusLastFocused()
+    local wf = hs.window.filter
+    local lastFocused = wf.defaultCurrentSpace:getWindows(wf.sortByFocusedLast)
+    if #lastFocused > 0 then lastFocused[1]:focus() end
+end
+
+-- Create the chooser.
+-- On selection, copy the emoji and type it into the focused application.
+local chooser = hs.chooser.new(function(choice)
+    if not choice then focusLastFocused(); return end
+    hs.pasteboard.setContents(choice["chars"])
+    focusLastFocused()
+    hs.eventtap.keyStrokes(hs.pasteboard.getContents())
+end)
+
+chooser:searchSubText(true)
+chooser:choices(choices)
+
+hs.hotkey.bind({"alt"}, ";", function() chooser:show() end)
