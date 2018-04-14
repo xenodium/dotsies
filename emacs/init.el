e96ec58b5db0eea7f766568b4c56dcd03d0bb092
xenodium <me+gh@xenodium.com>
2018-04-14 16:12:32 +0100

    Updating

--- emacs/init.el
+++ emacs/init.el
@@ -850,6 +850,7 @@ Values between 0 - 100."
                    ("http://sdegutis.com/blog/atom.xml" blog StevenDegutis)
                    ("http://blog.josephholsten.com/feed.xml" blog Libera-Ideoj)
                    ("http://prodissues.com/feed" blog Prodissues)
+                   ("http://irreal.org/blog/?feed=rss2" blog Irreal)
                    ("http://ben-evans.com/benedictevans?format=RSS" blog Ben-Evans)
                    ("http://ios-goodies.tumblr.com/rss" blog ios-goodies)
                    ("https://feeds.feedburner.com/codinghorror" blog Coding-Horror)
