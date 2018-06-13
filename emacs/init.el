681c471490e78a69f49b46754b55eccccd401cb5
xenodium <alvaro@xenodium.com>
2018-06-13 08:56:28 +0100

    Adding ar/ediff-dwim

--- emacs/init.el
+++ emacs/init.el
@@ -1234,7 +1234,56 @@ Optional argument NON-RECURSIVE to shallow-search."
               (when (eq major-mode 'org-mode)
                 (visible-mode +1)  ; default 0
                 (setq-local truncate-lines nil)  ; no `org-startup-truncated' in hook
-                (setq-local org-hide-leading-stars t)))))
+                (setq-local org-hide-leading-stars t))))
+
+  ;; From https://scripter.co/do-ediff-as-i-mean/
+  (defun ar/ediff-dwim ()
+    "Do ediff as I mean.
+
+If a region is active, call `ediff-regions-wordwise'.
+Else if the frame has 2 windows with identical major modes,
+  - Do `ediff-files' if the buffers are associated to files and the buffers
+    have not been modified.
+  - Do `ediff-buffers' otherwise.
+Else if the current is a file buffer with a VC backend, call `vc-ediff'
+Else call `ediff-buffers'."
+    (interactive)
+    (let* ((num-win (safe-length (window-list)))
+           (bufa (get-buffer (buffer-name)))
+           (filea (buffer-file-name bufa))
+           (modea (with-current-buffer bufa major-mode))
+           bufb fileb modeb)
+      (save-excursion
+        (other-window 1)
+        (setq bufb (get-buffer (buffer-name)))
+        (setq fileb (buffer-file-name bufb))
+        (setq modeb (with-current-buffer bufb major-mode)))
+      (cond
+       ;; If a region is selected
+       ((region-active-p)
+        (call-interactively #'ediff-regions-wordwise))
+       ;; Else if 2 windows with same major modes
+       ((and (= 2 num-win)
+             (eq modea modeb))
+        (if ;; If either of the buffers is not associated to a file,
+            ;; or if either of the buffers is modified
+            (or (null filea)
+                (null fileb)
+                (buffer-modified-p bufa)
+                (buffer-modified-p bufb))
+            (progn
+              (message "Running (ediff-buffers \"%s\" \"%s\") .." bufa bufb)
+              (ediff-buffers bufa bufb))
+          (progn
+            (message "Running (ediff-files \"%s\" \"%s\") .." filea fileb)
+            (ediff-files filea fileb))))
+       ;; Else if file in current buffer has a vc backend
+       ((and filea
+             (vc-registered filea))
+        (call-interactively #'vc-ediff))
+       ;; Else call `ediff-buffers'
+       (t
+        (call-interactively #'ediff-buffers))))))
 
 ;; ediff-revision cleanup.
 ;; From http://www.emacswiki.org/emacs/DavidBoon#toc8
