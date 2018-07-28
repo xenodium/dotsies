d943c48454c3ddd4d3634d77c2ac94683c78182a
xenodium <me+gh@xenodium.com>
2018-07-28 16:04:04 +0100

    More on newinit

--- emacs/newinit.el
+++ emacs/newinit.el
@@ -105,6 +105,7 @@
 
 (use-package validate
   :ensure t
+  :config
   (defalias 'vsetq 'validate-setq))
 
 ;;;; Init helpers - END
@@ -266,6 +267,40 @@
   (global-centered-cursor-mode +1))
 
 ;;;; Navigation END
+
+;;;; Editing START
+
+(use-package region-bindings-mode
+  :ensure t
+  :defer 2
+  :config
+  (region-bindings-mode-enable))
+
+(use-package multiple-cursors :ensure t
+  :after region-bindings-mode
+  :init
+  (global-unset-key (kbd "M-<down-mouse-1>"))
+  :bind (("C-c a" . mc/mark-all-like-this)
+         ("C-c n" . mc/mark-more-like-this-extended)
+         ("M-1" . mc/mark-next-like-this)
+         ("M-!" . mc/unmark-next-like-this)
+         ("M-2" . mc/mark-previous-like-this)
+         ("M-@" . mc/unmark-previous-like-this)
+         ("M-<mouse-1>" . mc/add-cursor-on-click))
+  :bind (:map region-bindings-mode-map
+              ("a" . mc/mark-all-like-this)
+              ("p" . mc/mark-previous-like-this)
+              ("n" . mc/mark-next-like-this)
+              ("P" . mc/unmark-previous-like-this)
+              ("N" . mc/unmark-next-like-this)
+              ("m" . mc/mark-more-like-this-extended)
+              ("h" . mc-hide-unmatched-lines-mode)
+              ("\\" . mc/vertical-align-with-space)
+              ("#" . mc/insert-numbers) ; use num prefix to set the starting number
+              ("^" . mc/edit-beginnings-of-lines)
+              ("$" . mc/edit-ends-of-lines)))
+
+;;;; Editing END
 
 ;;;; Buffers START
 (use-package menu-bar
