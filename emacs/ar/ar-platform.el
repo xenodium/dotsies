327f0619b6e22926a13df3e12f98a3826fe17c61
xenodium <me+gh@xenodium.com>
2018-07-29 21:23:34 +0100

    Migrating more hydras

--- emacs/ar/ar-platform.el
+++ emacs/ar/ar-platform.el
@@ -54,6 +54,56 @@ URL `http://ergoemacs.org/emacs/emacs_dired_open_file_in_ext_apps.html'"
       (when ξdo-it-p
         (mapc (ar/platform-open-in-external-app-function) ξfile-list)))))
 
+(defun ar/platform-open-file-at-point ()
+  "Open the file path at point.
+If there is text selection, uses the text selection for path.
+If the path starts with “http://”, open the URL in browser.
+Input path can be {relative, full path, URL}.
+Path may have a trailing “:‹n›” that indicates line number.
+If so, jump to that line number.
+If path does not have a file extention, automatically try with “.el” for elisp
+files.
+This command is similar to `find-file-at-point' but without prompting for
+confirmation.
+
+URL `http://ergoemacs.org/emacs/emacs_open_file_path_fast.html'"
+  (interactive)
+  (let ((ξpath (if (use-region-p)
+                   (buffer-substring-no-properties (region-beginning) (region-end))
+                 (let (p0 p1 p2)
+                   (validate-setq p0 (point))
+                   ;; chars that are likely to be delimiters of full path, e.g. space, tabs, brakets.
+                   (skip-chars-backward "^  \"\t\n`'|()[]{}<>〔〕“”〈〉《》【】〖〗«»‹›·。\\`")
+                   (validate-setq p1 (point))
+                   (goto-char p0)
+                   (skip-chars-forward "^  \"\t\n`'|()[]{}<>〔〕“”〈〉《》【】〖〗«»‹›·。\\'")
+                   (validate-setq p2 (point))
+                   (goto-char p0)
+                   (buffer-substring-no-properties p1 p2)))))
+    (if (string-match-p "\\`https?://" ξpath)
+        (browse-url ξpath)
+      (progn ; not starting “http://”
+        (if (string-match "^\\`\\(.+?\\):\\([0-9]+\\)\\'" ξpath)
+            (progn
+              (let (
+                    (ξfpath (match-string 1 ξpath))
+                    (ξline-num (string-to-number (match-string 2 ξpath))))
+                (if (file-exists-p ξfpath)
+                    (progn
+                      (find-file ξfpath)
+                      (goto-char 1)
+                      (forward-line (1- ξline-num)))
+                  (progn
+                    (when (y-or-n-p (format "File doesn't exist: %s.  Create? " ξfpath))
+                      (find-file ξfpath))))))
+          (progn
+            (if (file-exists-p ξpath)
+                (find-file ξpath)
+              (if (file-exists-p (concat ξpath ".el"))
+                  (find-file (concat ξpath ".el"))
+                (when (y-or-n-p (format "File doesn't exist: %s.  Create? " ξpath))
+                  (find-file ξpath ))))))))))
+
 (provide 'ar-platform)
 
 ;;; ar-platform.el ends here
--- emacs/newinit.el
+++ emacs/newinit.el
@@ -638,6 +638,18 @@ already narrowed."
 
 ;;;; Helm END
 
+;;;; Less important START
+
+(use-package ar-platform
+  :commands (ar/platform-open-in-external-app
+             ar/platform-new-browser-tab
+             ar/platform-open-file-at-point))
+
+(use-package ar-file
+  :commands (ar/file-open-closest-build-file))
+
+;;;; Less important END
+
 ;;;; Hydra START
 (use-package hydra
   :ensure t
@@ -661,8 +673,56 @@ Quick insert: _w_eb bookmark _b_acklog bookmark
     ("t" ar/org-add-todo nil)
     ("d" ar/org-add-done nil)
     ("q" nil nil :color blue))
+
+  (defun ar/hydra-open-dwim ()
+    "Choose \"open\" hydra based on current mode."
+    (interactive)
+    (cond ((derived-mode-p 'c-mode) (hydra-open-c-mode/body))
+          ((derived-mode-p 'prog-mode) (hydra-open-prog-mode/body))
+          (t (hydra-open/body))))
+
+  (defhydra hydra-open-c-mode (:color blue)
+    "open"
+    ("o" ff-find-other-file "other")
+    ("e" ar/platform-open-in-external-app "externally")
+    ("u" ar/platform-open-file-at-point "url at point")
+    ("b" ar/file-open-closest-build-file "build file")
+    ("q" nil "cancel"))
+
+  (defhydra hydra-open (:color blue)
+    "
+Open: _p_oint _e_xternally
+"
+    ("e" ar/platform-open-in-external-app nil)
+    ("p" ar/platform-open-file-at-point nil)
+    ("q" nil "cancel"))
+
+  (defhydra hydra-open-prog-mode (:color blue)
+    "open"
+    ("o" ff-find-other-file "other")
+    ("e" ar/platform-open-in-external-app "externally")
+    ("u" ar/platform-open-file-at-point "url at point")
+    ("b" ar/file-open-closest-build-file "build file")
+    ("q" nil "cancel"))
+
+  (defhydra hydra-git-gutter (:pre (git-gutter-mode +1))
+    "
+Git: _n_ext     _s_tage  _d_iff
+     _p_revious _k_ill _q_uit
+"
+    ("n" git-gutter:next-hunk nil)
+    ("p" git-gutter:previous-hunk nil)
+    ("s" git-gutter:stage-hunk nil)
+    ("k" (lambda ()
+           (interactive)
+           (git-gutter:revert-hunk)
+           (call-interactively #'git-gutter:next-hunk)) nil)
+    ("d" git-gutter:popup-hunk nil)
+    ("q" nil nil :color blue))
   :bind (("C-c s" . hydra-search/body)
-         ("C-c x" . hydra-quick-insert/body)))
+         ("C-c x" . hydra-quick-insert/body)
+         ("C-c o" . ar/hydra-open-dwim)
+         ("C-c g" . hydra-git-gutter/body)))
 ;;;; Hydra END
 
 ;;;; Buffers START
