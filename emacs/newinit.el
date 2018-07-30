c78edac9fb917a75af231106e36ea67f0b48b11c
xenodium <alvaro@xenodium.com>
2018-07-31 00:41:05 +0100

    Removing add-hook! from doom

--- emacs/newinit.el
+++ emacs/newinit.el
@@ -115,81 +115,6 @@
 
 ;;;; Init helpers END
 
-;;;; Doom imports START
-
-(defun doom-unquote (exp)
-  "Return EXP unquoted."
-  (while (memq (car-safe exp) '(quote function))
-    (setq exp (cadr exp)))
-  exp)
-
-(defun doom-enlist (exp)
-  "Return EXP wrapped in a list, or as-is if already a list."
-  (if (listp exp) exp (list exp)))
-
-(defun doom--resolve-hook-forms (hooks)
-  (cl-loop with quoted-p = (eq (car-safe hooks) 'quote)
-           for hook in (doom-enlist (doom-unquote hooks))
-           if (eq (car-safe hook) 'quote)
-           collect (cadr hook)
-           else if quoted-p
-           collect hook
-           else collect (intern (format "%s-hook" (symbol-name hook)))))
-
-(defmacro add-hook! (&rest args)
-  "A convenience macro for `add-hook'. Takes, in order:
-  1. Optional properties :local and/or :append, which will make the hook
-     buffer-local or append to the list of hooks (respectively),
-  2. The hooks: either an unquoted major mode, an unquoted list of major-modes,
-     a quoted hook variable or a quoted list of hook variables. If unquoted, the
-     hooks will be resolved by appending -hook to each symbol.
-  3. A function, list of functions, or body forms to be wrapped in a lambda.
-Examples:
-    (add-hook! 'some-mode-hook 'enable-something)
-    (add-hook! some-mode '(enable-something and-another))
-    (add-hook! '(one-mode-hook second-mode-hook) 'enable-something)
-    (add-hook! (one-mode second-mode) 'enable-something)
-    (add-hook! :append (one-mode second-mode) 'enable-something)
-    (add-hook! :local (one-mode second-mode) 'enable-something)
-    (add-hook! (one-mode second-mode) (setq v 5) (setq a 2))
-    (add-hook! :append :local (one-mode second-mode) (setq v 5) (setq a 2))
-Body forms can access the hook's arguments through the let-bound variable
-`args'."
-  (declare (indent defun) (debug t))
-  (let ((hook-fn 'add-hook)
-        append-p local-p)
-    (while (keywordp (car args))
-      (pcase (pop args)
-        (:append (setq append-p t))
-        (:local  (setq local-p t))
-        (:remove (setq hook-fn 'remove-hook))))
-    (let ((hooks (doom--resolve-hook-forms (pop args)))
-          (funcs
-           (let ((val (car args)))
-             (if (memq (car-safe val) '(quote function))
-                 (if (cdr-safe (cadr val))
-                     (cadr val)
-                   (list (cadr val)))
-               (list args))))
-          forms)
-      (dolist (fn funcs)
-        (setq fn (if (symbolp fn)
-                     `(function ,fn)
-                   `(lambda (&rest _) ,@args)))
-        (dolist (hook hooks)
-          (push (if (eq hook-fn 'remove-hook)
-                    `(remove-hook ',hook ,fn ,local-p)
-                  `(add-hook ',hook ,fn ,append-p ,local-p))
-                forms)))
-      `(progn ,@(nreverse forms)))))
-
-(defmacro remove-hook! (&rest args)
-  "Convenience macro for `remove-hook'. Takes the same arguments as
-`add-hook!'."
-  `(add-hook! :remove ,@args))
-
-;;;; Doom imports END
-
 ;;;; Appearance START
 
 ;; Get rid of splash screens.
@@ -247,64 +172,65 @@ Body forms can access the hook's arguments through the let-bound variable
 
 ;;;; Appearance END
 
-(add-hook! 'emacs-startup-hook
-  ;; Undo GC values post init.el.
-  (vsetq gc-cons-threshold 16777216
-         gc-cons-percentage 0.1)
-  (run-with-idle-timer 5 t #'garbage-collect)
-  (vsetq garbage-collection-messages t)
-  (setq file-name-handler-alist ar/init--file-name-handler-alist)
+(add-hook
+ 'emacs-startup-hook
+ (lambda ()
+   ;; Undo GC values post init.el.
+   (vsetq gc-cons-threshold 16777216
+          gc-cons-percentage 0.1)
+   (run-with-idle-timer 5 t #'garbage-collect)
+   (vsetq garbage-collection-messages t)
+   (setq file-name-handler-alist ar/init--file-name-handler-alist)
 
-  (message "Emacs ready in %s with %d garbage collections."
-           (format "%.2f seconds"
-                   (float-time
-                    (time-subtract after-init-time before-init-time)))
-           gcs-done)
+   (message "Emacs ready in %s with %d garbage collections."
+            (format "%.2f seconds"
+                    (float-time
+                     (time-subtract after-init-time before-init-time)))
+            gcs-done)
 
-  ;; Additional load paths.
-  (add-to-list 'load-path "~/.emacs.d/ar")
-  (add-to-list 'load-path "~/.emacs.d/local")
-  (add-to-list 'load-path "~/.emacs.d/external")
+   ;; Additional load paths.
+   (add-to-list 'load-path "~/.emacs.d/ar")
+   (add-to-list 'load-path "~/.emacs.d/local")
+   (add-to-list 'load-path "~/.emacs.d/external")
 
+   (use-package server
+     :defer 10
+     :config
+     (unless (server-running-p)
+       (server-start)))
 
-  (use-package server
-    :defer 10
-    :config
-    (unless (server-running-p)
-      (server-start)))
+   (use-package s
+     :ensure t)
 
-  (use-package s
-    :ensure t)
-
-  ;; Ask shell for PATH, MANPATH, and exec-path and update Emacs environment.
-  ;; We do this early on as we assert binaries are installed throughout
-  ;; init.
-  (use-package exec-path-from-shell
-    :ensure t
-    :config
-    (exec-path-from-shell-initialize))
-  (load "~/.emacs.d/features/ivy.el")
-  (load "~/.emacs.d/features/maintenance.el")
-  (load "~/.emacs.d/features/ui.el")
-  (load "~/.emacs.d/features/files.el")
-  (load "~/.emacs.d/features/editing.el")
-  (load "~/.emacs.d/features/git.el")
-  (load "~/.emacs.d/features/navigation.el")
-  (load "~/.emacs.d/features/platform.el")
-  (load "~/.emacs.d/features/helm.el")
-  (load "~/.emacs.d/features/file.el")
-  (load "~/.emacs.d/features/hydra.el")
-  (load "~/.emacs.d/features/eshell.el")
-  (load "~/.emacs.d/features/org.el")
-  (load "~/.emacs.d/features/dired.el")
-  (load "~/.emacs.d/features/dev.el")
-  (load "~/.emacs.d/features/company.el")
-  (load "~/.emacs.d/features/objc.el")
-  (load "~/.emacs.d/features/elfeed.el")
-  (load "~/.emacs.d/features/modal.el")
-  (load "~/.emacs.d/features/mail.el")
-  (dolist (file (file-expand-wildcards "~/.emacs.d/work/*.el"))
-    (load file)))
+   ;; Ask shell for PATH, MANPATH, and exec-path and update Emacs environment.
+   ;; We do this early on as we assert binaries are installed throughout
+   ;; init.
+   (use-package exec-path-from-shell
+     :ensure t
+     :config
+     (exec-path-from-shell-initialize))
+   (load "~/.emacs.d/features/ivy.el")
+   (load "~/.emacs.d/features/maintenance.el")
+   (load "~/.emacs.d/features/ui.el")
+   (load "~/.emacs.d/features/files.el")
+   (load "~/.emacs.d/features/editing.el")
+   (load "~/.emacs.d/features/git.el")
+   (load "~/.emacs.d/features/navigation.el")
+   (load "~/.emacs.d/features/platform.el")
+   (load "~/.emacs.d/features/helm.el")
+   (load "~/.emacs.d/features/file.el")
+   (load "~/.emacs.d/features/hydra.el")
+   (load "~/.emacs.d/features/eshell.el")
+   (load "~/.emacs.d/features/org.el")
+   (load "~/.emacs.d/features/dired.el")
+   (load "~/.emacs.d/features/dev.el")
+   (load "~/.emacs.d/features/company.el")
+   (load "~/.emacs.d/features/objc.el")
+   (load "~/.emacs.d/features/elfeed.el")
+   (load "~/.emacs.d/features/modal.el")
+   (load "~/.emacs.d/features/mail.el")
+   (dolist (file (file-expand-wildcards "~/.emacs.d/work/*.el"))
+     (load file))))
 
 (provide 'init)
 ;;; init.el ends here
