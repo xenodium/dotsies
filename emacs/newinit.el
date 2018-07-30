d5b8821155173079c211d639b56015f85bd0a2bb
xenodium <me+gh@xenodium.com>
2018-07-30 14:13:08 +0100

    Adding minimal Objc

--- emacs/newinit.el
+++ emacs/newinit.el
@@ -1153,6 +1153,67 @@ Git: _n_ext     _s_tage  _d_iff
 
 ;;;; Dired END
 
+;;;; Company completion START
+
+(use-package company
+  :ensure t
+  :commands (company-mode
+             global-company-mode company-complete
+             company-complete-common
+             company-manual-begin
+             company-grab-line)
+  :bind (:map global-map
+              ("<backtab>" . company-complete)
+              :map company-search-map
+              ("C-n" . company-select-next)
+              ("C-p" . company-select-previous)
+              :map company-active-map
+              ("C-l" . company-show-location)
+              ("C-s" . company-filter-candidates)
+              ("C-d" . company-show-doc-buffer)
+              ("C-n" . company-select-next)
+              ("C-p" . company-select-previous))
+  :config
+  (use-package company-dabbrev
+    :config
+    (vsetq company-dabbrev-downcase nil)
+    (vsetq company-dabbrev-ignore-case nil))
+
+  (use-package company-dabbrev-code
+    :config
+    (vsetq company-dabbrev-code-ignore-case nil))
+
+  (vsetq company-idle-delay 0.2)
+  (vsetq company-show-numbers t)
+  (vsetq company-minimum-prefix-length 2)
+  (vsetq company-tooltip-align-annotations t)
+
+  ;; Disable all company backends by default.
+  (csetq company-backends '()))
+
+;;;; Company completion END
+
+;;;; Objective-C START
+
+(use-package objc-mode
+  :init
+  (defun ar/objc-mode-hook-function ()
+    "Called when entering `objc-mode'."
+    ;; Hook is run twice. Avoid:
+    ;; http://debbugs.gnu.org/cgi/bugreport.cgi?bug=16759
+    (unless (boundp 'objc-mode-hook-did-run)
+      (set-fill-column 100)
+      (company-mode +1)
+      (setq-local company-backends '((company-files
+                                      company-keywords)))
+      (setq-local objc-mode-hook-did-run t)))
+  :hook (objc-mode . ar/objc-mode-hook-function)
+  :bind (:map objc-mode-map
+              ([f6] . recompile)))
+
+;;;; Objective-C END
+
+
 ;;;; Elfeed START
 
 (add-hook! 'emacs-startup-hook
