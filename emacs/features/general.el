c4216bd0212dc8fbc052da2fff07a5e70597102a
xenodium <alvaro@xenodium.com>
2018-07-31 21:34:03 +0100

    Ignore running processes when exiting in newinit

---
+++ emacs/features/general.el
@@ -0,0 +1,9 @@
+(use-package cl
+  :config
+  ;; Ignore running processes when closing Emacs
+  ;; From http://oremacs.com/2015/01/04/dired-nohup
+  (defadvice save-buffers-kill-emacs
+      (around no-query-kill-emacs activate)
+    "Prevent \"Active processes exist\" query on exit."
+    (cl-letf (((symbol-function #'process-list) (lambda ())))
+      ad-do-it)))
