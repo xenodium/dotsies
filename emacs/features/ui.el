dad7253e1302246d168ac721717bbb3a2f7f4eb1
xenodium <alvaro@xenodium.com>
2018-07-31 01:09:09 +0100

    Removing non-core loading from newinit

--- emacs/features/ui.el
+++ emacs/features/ui.el
@@ -1,3 +1,44 @@
+;; Ensure window is maximized after window setup.
+(use-package maxframe
+  :ensure t
+  :hook (window-setup . maximize-frame))
+
+(use-package base16-theme
+  :ensure t
+  :config
+  (load-theme 'base16-atelier-heath t)
+  (let ((line (face-attribute 'mode-line :underline)))
+    (set-face-attribute 'mode-line          nil :overline   line)
+    (set-face-attribute 'mode-line-inactive nil :overline   line)
+    (set-face-attribute 'mode-line-inactive nil :underline  line)
+    (set-face-attribute 'mode-line          nil :box        nil)
+    (set-face-attribute 'mode-line-inactive nil :box        nil)
+    (set-face-attribute 'mode-line-inactive nil :background "#695d69")))
+
+(use-package moody
+  :ensure t
+  :config
+  (setq x-underline-at-descent-line t)
+
+  (setq-default mode-line-format
+                '(" "
+                  mode-line-front-space
+                  mode-line-client
+                  mode-line-frame-identification
+                  mode-line-buffer-identification " " mode-line-position
+                  (vc-mode vc-mode)
+                  (multiple-cursors-mode mc/mode-line)
+                  " " mode-line-modes
+                  mode-line-end-spaces))
+
+  (use-package minions
+    :ensure t
+    :config
+    (minions-mode +1))
+
+  (moody-replace-mode-line-buffer-identification)
+  (moody-replace-vc-mode))
+
 ;; Use y/n instead of yes/no confirms.
 ;; From http://pages.sachachua.com/.emacs.d/Sacha.html#sec-1-4-8
 (fset 'yes-or-no-p 'y-or-n-p)
