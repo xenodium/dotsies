6f31fa4bbeb40736c37e6dfe3362d402dcee3657
xenodium <me+gh@xenodium.com>
2018-08-01 22:37:51 +0100

    Recognize ObjC headers

--- emacs/features/objc.el
+++ emacs/features/objc.el
@@ -6,11 +6,18 @@
     ;; http://debbugs.gnu.org/cgi/bugreport.cgi?bug=16759
     (unless (boundp 'objc-mode-hook-did-run)
       (set-fill-column 100)
-      (company-mode +1)
-      (subword-mode +1)
+
+      ;; Company backends for Objective-C.
       (setq-local company-backends '((company-files
                                       company-keywords)))
+      (company-mode +1)
+
+      ;; Userful for camel-case Objective-C.
+      (subword-mode +1)
+
+      ;; Format files for me.
       (add-hook 'before-save-hook #'clang-format-buffer t t)
+
       (setq-local objc-mode-hook-did-run t)))
   :hook (objc-mode . ar/objc-mode-hook-function)
   :bind (:map objc-mode-map
@@ -18,3 +25,7 @@
   :config
   (use-package clang-format
     :ensure t))
+
+;; Recognize .h headers can also be Objective-C (enable objc-mode for them).
+(use-package dummy-h-mode
+  :mode (("\\.h\\'" . dummy-h-mode)))
