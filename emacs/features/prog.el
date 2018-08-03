1ef30340ff515ac3631e5bb0120753edc07db659
xenodium <me+gh@xenodium.com>
2018-08-03 22:49:57 +0100

    Rejigging features on newinit

--- emacs/features/prog.el
+++ emacs/features/prog.el
@@ -1,8 +1,10 @@
 (use-package prog-mode
   :hook ((prog-mode . company-mode)
+         (prog-mode . flycheck-mode)
          (prog-mode . flyspell-prog-mode))
   :config
-  (use-package flyspell)
+  (require 'flyspell)
+  (require 'flycheck)
 
   ;; Highlight hex strings in respective color.
   (use-package rainbow-mode :ensure t))
