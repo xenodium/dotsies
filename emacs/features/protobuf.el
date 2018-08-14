b827c683d7536e562205d573d1f6d43c34fd45e9
xenodium <me+gh@xenodium.com>
2018-08-14 10:11:41 +0100

    Re-adding protobuf-mode back in

---
+++ emacs/features/protobuf.el
@@ -0,0 +1,15 @@
+(use-package protobuf-mode
+  :ensure t
+  :mode ("\\.proto\\'" . protobuf-mode))
+  :config
+  (defun ar/reindex-proto-fields ()
+    "From within a proto message, reindex all proto field tags."
+    (interactive)
+    (save-excursion
+      (save-restriction
+        (narrow-to-defun)
+        (goto-char (point-min))
+        (let ((counter 1))
+          (while (search-forward-regexp "\\(\\(\\(optional\\)\\|\\(required\\)\\).*= *\\)[1-9]+" nil t)
+            (replace-match (format "\\1%d" counter) t nil)
+            (setq counter (+ counter 1))))))))
