77fab2e19131bbfbb87279697e61b832c6c8141c
xenodium <me+gh@xenodium.com>
2018-08-19 16:33:43 +0100

    Re-adding helpful to init.el

--- emacs/features/help.el
+++ emacs/features/help.el
@@ -4,3 +4,9 @@
   :config
   ;; Select help window by default.
   (ar/vsetq help-window-select t))
+
+(use-package helpful
+  :ensure t
+  :bind  (("C-h f" . helpful-callable)
+          ("C-h v" . helpful-variable)
+          ("C-h k" . helpful-key)))
