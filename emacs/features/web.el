d69bbbc08b5dca4b1cf54e02289522b6de061b28
xenodium <alvaro@xenodium.com>
2018-08-22 01:18:20 +0100

    Using <RET> instead of C-c <RET> for goto-address-at-point/

--- emacs/features/web.el
+++ emacs/features/web.el
@@ -3,7 +3,10 @@
   :config
   (use-package enlive :ensure t))
 
-;; Make URLs and e-mail addresses clickable.
+;; Make URLs and e-mail addresses clickable or activatable wit <RET>.
 (use-package goto-addr
+  :bind (:map goto-address-highlight-keymap
+              ("<RET>" . goto-address-at-point)
+              ("M-<RET>" . newline))
   :commands (goto-address-prog-mode
              goto-address-mode))
