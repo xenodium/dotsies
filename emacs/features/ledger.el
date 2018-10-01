386636c8f838a42d46814d0876e70b0f0dfd5be8
xenodium <me+gh@xenodium.com>
2018-10-01 22:25:47 +0100

    Adding flycheck-ledger

--- emacs/features/ledger.el
+++ emacs/features/ledger.el
@@ -1,3 +1,10 @@
+(require 'ar-vsetq)
+
 (use-package ledger-mode
   :ensure t
-  :ensure-system-package ledger)
+  :ensure-system-package ledger
+  :config
+  (ar/vsetq ledger-reconcile-default-commodity "GBP")
+  (use-package flycheck-ledger
+    :ensure t
+    :hook (ledger-mode . flycheck-mode)))
