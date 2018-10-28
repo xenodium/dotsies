19d3d63f2742dfee2eb0e293168087b90e2f4fce
xenodium <me+gh@xenodium.com>
2018-10-28 18:06:07 +0000

    Adding company completion to ledger

--- emacs/features/ledger.el
+++ emacs/features/ledger.el
@@ -3,8 +3,14 @@
 (use-package ledger-mode
   :ensure t
   :ensure-system-package ledger
+  :hook ((ledger-mode . company-mode)
+         (ledger-mode . ar/ledger-mode-hook-function))
   :config
   (ar/vsetq ledger-reconcile-default-commodity "GBP")
+
+  (defun ar/ledger-mode-hook-function ()
+    (setq-local company-backends '((company-capf))))
+
   (use-package flycheck-ledger
     :ensure t
-    :hook (ledger-mode . flycheck-mode)))
+    :hook ((ledger-mode . flycheck-mode))))
