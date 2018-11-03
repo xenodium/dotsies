cdf9e7b04e7aa7788b9a3a13639facfaabbac04c
xenodium <me+gh@xenodium.com>
2018-11-03 10:46:18 +0000

    Adding ar/feed-for-url-in-clipboard and new feeds

--- emacs/features/elfeed.el
+++ emacs/features/elfeed.el
@@ -59,19 +59,7 @@
 
   (ar/vsetq elfeed-feeds
             '(
-              ("https://ryanholiday.net/feed/" blog meditation RyanHoliday)
-              ("https://ryanholiday.net/comments/feed/" blog meditation RyanHolidayComments)
-              ("https://www.madfientist.com/feed/" blog money MadFientist)
-              ("https://www.madfientist.com/comments/feed/" blog money MadFientistComments)
-              ("https://www.getrichslowly.org/feed" blog money GetRichSlowly)
-              ("https://www.choosefi.com/feed/" blog money Choosefi)
-              ("https://www.choosefi.com/category/podcast-episodes/feed" blog money ChoosefiPodcasts)
-              ("https://affordanything.com/blog/feed" blog money AffordAnything)
-              ("https://affordanything.com/comments/feed/" blog money AffordAnything)
-              ("http://feeds.feedburner.com/AffordAnythingFeed" blog money AffordAnything)
               ("http://200ok.ch/atom.xml" blog emacs tech 200ok)
-              ("https://changelog.com/feed" blog tech news)
-              ("http://tangent.libsyn.com" blog money  ChristopherRyan)
               ("http://akkartik.name/feeds.xml" blog tech KartikAgaram)
               ("http://ben-evans.com/benedictevans?format=RSS" blog tech Ben-Evans)
               ("http://blog.abhixec.com/index.xml" blog emacs RandomMusings)
@@ -87,6 +75,7 @@
               ("http://feeds.bbci.co.uk/news/uk/rss.xml?edition=uk" news BBCUK bbc)
               ("http://feeds.bbci.co.uk/news/world/rss.xml?edition=uk" news BBCWorld bbc)
               ("http://feeds.feedblitz.com/atomicspin&x=1" blog dev emacs AtomicObject)
+              ("http://feeds.feedburner.com/AffordAnythingFeed" blog money AffordAnything)
               ("http://feeds.feedburner.com/FinancialSamurai" blog money FinancialSamurai)
               ("http://feeds.feedburner.com/japaneseruleof7" blog japan japanese-rule-of-7)
               ("http://francismurillo.github.io/hacker/feed.xml" blog tech emacs francismurillo)
@@ -101,6 +90,7 @@
               ("http://rubyronin.com/wp-feed.php" blog japan the-ruby-ronin)
               ("http://sachachua.com/blog/feed" blog emacs tech sachachua)
               ("http://sdegutis.com/blog/atom.xml" blog tech StevenDegutis)
+              ("http://tangent.libsyn.com" blog money  ChristopherRyan)
               ("http://tech.memoryimprintstudio.com/feed" blog emacs tech MemoryImprintStudio)
               ("http://www.arcadianvisions.com/blog/rss.xml" blog tech emacs arcadianvisions)
               ("http://www.badykov.com/feed.xml" blog emacs KrakenOfThought)
@@ -110,12 +100,15 @@
               ("http://www.sastibe.de/index.xml" blog emacs SebastianSchweer)
               ("http://www.thisiscolossal.com/feed" blog tech Colossal)
               ("http://zzamboni.org/index.xml" blog hammerspoon tech Diego-Martín-Zamboni)
+              ("https://affordanything.com/blog/feed" blog money AffordAnything)
+              ("https://affordanything.com/comments/feed/" blog money AffordAnything)
               ("https://ambrevar.xyz/rss.xml" blog ambrevar)
               ("https://ambrevar.xyz/rss.xml" blog emacs PierreNeidhardt)
               ("https://babbagefiles.blogspot.com/feeds/posts/default" blog emacs tech)
               ("https://blog.burntsushi.net/index.xml" blog dev BurnedSushi)
               ("https://blog.danielgempesaw.com/rss" blog emacs tech DanielGempesaw)
               ("https://blog.moneysavingexpert.com/blog.rss" blog money MoneySavingExpert MartinLewis)
+              ("https://changelog.com/feed" blog tech news)
               ("https://changelog.complete.org/feed" blog emacs tech JohnGoerzen)
               ("https://colelyman.com/index.xml" blog emacs ColeLyman)
               ("https://copyninja.info/feeds/all.atom.xml" blog tech dev copyninja)
@@ -139,16 +132,18 @@
               ("https://manuel-uberti.github.io/feed.xml" blog emacs ManuelUberti)
               ("https://martinralbrecht.wordpress.com/feed" blog emacs tech MartinAlbrecht)
               ("https://matt.hackinghistory.ca/feed/" blog emacs MattPrice)
-              ("https://medium.com/feed/@mwfogleman" blog tech emacs meditation MichaelFogleman)
+              ("https://medium.com/feed/@tasshin" blog tech emacs meditation MichaelFogleman)
               ("https://news.ycombinator.com/rss" news hackernews tech)
               ("https://ogbe.net/blog.xml" blog emacs tech DennisOgbe)
               ("https://palikar.github.io/index.xml" blog emacs StanislavArnaudov)
               ("https://piware.de/post/index.xml" blog tech)
               ("https://punchagan.muse-amuse.in/feed.xml" blog emacs Punchagan)
+              ("https://ryanholiday.net/comments/feed/" blog meditation RyanHolidayComments)
+              ("https://ryanholiday.net/feed/" blog meditation RyanHoliday)
+              ("https://ryanholiday.net/feed/atom" blog emacs StringsNote)
               ("https://sam217pa.github.io/index.xml" blog emacs BacterialFinches)
               ("https://sciencebasedmedicine.org/feed" blog medicine ScienceBasedMedicine)
               ("https://scripter.co/posts/index.xml" blog emacs tech dev)
-              ("https://stingh711.github.io/feed.xml" blog emacs StringsNote)
               ("https://swiftnews.curated.co/issues.rss" blog swift tech ShiftNewsCurated)
               ("https://swiftweekly.github.io/feed.xml" blog swift tech SwiftWeekly)
               ("https://thesavingninja.com/feed" blog money SavingNinja)
@@ -158,10 +153,17 @@
               ("https://writequit.org/posts.xml" blog tech emacs writequit)
               ("https://www.baty.net/index.xml" blog emacs JackBaty)
               ("https://www.bytedude.com/feed.xml" blog emacs MarcinS)
-              ("https://www.campfirefinance.com/fire-feed/" blog money CampFireFinance)
+              ("https://www.campfirefinance.com/feed" blog money CampFireFinance)
+              ("https://www.choosefi.com/category/podcast-episodes/feed" blog money ChoosefiPodcasts)
+              ("https://www.choosefi.com/feed/" blog money Choosefi)
+              ("https://www.drweil.com/blog/health-tips/feed" blog health healthTips DrWeil)
+              ("https://www.drweil.com/blog/spontaneous-happiness/feed" blog health happinessTips DrWeil)
+              ("https://www.getrichslowly.org/feed" blog money GetRichSlowly)
               ("https://www.hasecke.eu/index.xml" blog emacs tech hasecke)
               ("https://www.ict4g.net/adolfo/site.atom" blog tech dev Adolfo)
               ("https://www.johndcook.com/blog/feed" blog emacs JohnDCook)
+              ("https://www.madfientist.com/comments/feed/" blog money MadFientistComments)
+              ("https://www.madfientist.com/feed/" blog money MadFientist)
               ("https://www.moneysavingexpert.com/news/feeds/news.rss" blog money MoneySavingExpert news)
               ("https://www.ogre.com/blog/feed" blog dev Ogre)
               ("https://www.reddit.com/r/UKPersonalFinance/.rss" social reddit UKPersonalFinance)
@@ -186,3 +188,54 @@
     "Filter the elfeed-search buffer to show news-tagged feeds."
     (interactive)
     (ar/elfeed-view-filtered "@6-months-ago +unread +news")))
+
+(require 'cl-macs)
+
+(use-package org-web-tools
+  :ensure t)
+
+(use-package esxml
+  :ensure t
+  :config)
+
+(require 'cl-macs)
+
+;; From https://github.com/alphapapa/unpackaged.el#feed-for-url
+(cl-defun ar/feed-for-url-in-clipboard (url &key (prefer 'atom) (all nil))
+  "Return feed URL for web page at URL.
+Interactively, insert the URL at point.  PREFER may be
+`atom' (the default) or `rss'.  When ALL is non-nil, return all
+feed URLs of all types; otherwise, return only one feed URL,
+preferring the preferred type."
+  (interactive (list (org-web-tools--get-first-url)))
+  (require 'esxml-query)
+  (require 'org-web-tools)
+  (cl-flet ((feed-p (type)
+                    ;; Return t if TYPE appears to be an RSS/ATOM feed
+                    (string-match-p (rx "application/" (or "rss" "atom") "+xml")
+                                    type)))
+    (let* ((preferred-type (format "application/%s+xml" (symbol-name prefer)))
+           (html (org-web-tools--get-url url))
+           (dom (with-temp-buffer
+                  (insert html)
+                  (libxml-parse-html-region (point-min) (point-max))))
+           (potential-feeds (esxml-query-all "link[rel=alternate]" dom))
+           (return (if all
+                       ;; Return all URLs
+                       (cl-loop for (tag attrs) in potential-feeds
+                                when (feed-p (alist-get 'type attrs))
+                                collect (url-expand-file-name (alist-get 'href attrs) url))
+                     (or
+                      ;; Return the first URL of preferred type
+                      (cl-loop for (tag attrs) in potential-feeds
+                               when (equal preferred-type (alist-get 'type attrs))
+                               return (url-expand-file-name (alist-get 'href attrs) url))
+                      ;; Return the first URL of non-preferred type
+                      (cl-loop for (tag attrs) in potential-feeds
+                               when (feed-p (alist-get 'type attrs))
+                               return (url-expand-file-name (alist-get 'href attrs) url))))))
+      (if (called-interactively-p)
+          (insert (if (listp return)
+                      (s-join " " return)
+                    return))
+        return))))
