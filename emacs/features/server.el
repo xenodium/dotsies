5cbb117b3eff885feb9eddf3234e03f9c658ebfb
xenodium <me+gh@xenodium.com>
2018-11-04 21:14:56 +0000

    Adding ar/emacs-server-file

--- emacs/features/server.el
+++ emacs/features/server.el
@@ -3,3 +3,7 @@
   :config
   (unless (server-running-p)
     (server-start)))
+
+(defun ar/emacs-server-file ()
+  "Emacs server file"
+  (nth 0 (split-string (shell-command-to-string "lsof -c Emacs | grep server | tr -s ' ' | cut -d' ' -f 8") "\n")))
