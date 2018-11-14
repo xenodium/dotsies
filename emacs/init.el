4b4cdfb49fdc8a7d91139443f7e40189a1f1652f
xenodium <me+gh@xenodium.com>
2018-11-14 08:02:32 +0000

    Cleanup

--- emacs/init.el
+++ emacs/init.el
@@ -27,16 +27,20 @@
 (setq mode-line-format nil)
 
 ;; No scrollbar by default.
-(when (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
+(when (fboundp 'scroll-bar-mode)
+  (scroll-bar-mode -1))
 
 ;; No nenubar by default.
-(when (fboundp 'menu-bar-mode) (menu-bar-mode -1))
+(when (fboundp 'menu-bar-mode)
+  (menu-bar-mode -1))
 
 ;; No toolbar by default.
-(when (fboundp 'tool-bar-mode) (tool-bar-mode -1))
+(when (fboundp 'tool-bar-mode)
+  (tool-bar-mode -1))
 
 ;; No tooltip by default.
-(when (fboundp 'tooltip-mode) (tooltip-mode -1))
+(when (fboundp 'tooltip-mode)
+  (tooltip-mode -1))
 
 ;; No Alarms by default.
 (setq ring-bell-function 'ignore)
