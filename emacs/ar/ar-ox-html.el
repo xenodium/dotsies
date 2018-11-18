f4238ad2dac42ddd3f40bd96b966b420d58cdf8b
xenodium <me+gh@xenodium.com>
2018-11-18 22:49:04 +0000

    Disabling fontify-face-mode in elisp mode

--- emacs/ar/ar-ox-html.el
+++ emacs/ar/ar-ox-html.el
@@ -65,9 +65,7 @@ Remove angle brackets: <06 February 2016> => 06 February 2016"
   "Export blog to HTML."
   (interactive)
   (let ((whitespace-line-foreground (face-attribute 'whitespace-line :foreground))
-        (whitespace-line-background (face-attribute 'whitespace-line :background))
-        ;; Disables fontify-face-mode while exporting.
-        (fontify-face-keywords '()))
+        (whitespace-line-background (face-attribute 'whitespace-line :background)))
     ;; Unset face (disable whitespace mode prior to export).
     (set-face-attribute 'whitespace-line nil
                         :foreground nil
