f51ec18db9db7bf3d313e5b0bf01bef866254f9a
xenodium <me+gh@xenodium.com>
2018-12-28 17:04:19 +0000

    Adding flycheck-inline

--- emacs/features/flycheck.el
+++ emacs/features/flycheck.el
@@ -1,6 +1,11 @@
-(use-package flycheck :ensure t
+(use-package flycheck
+  :ensure t
   :config
   ;; Override default flycheck triggers
   (ar/vsetq flycheck-check-syntax-automatically
-                 '(save idle-change mode-enabled)
-                 flycheck-idle-change-delay 0.8))
+            '(save idle-change mode-enabled)
+            flycheck-idle-change-delay 0.8)
+
+  (use-package flycheck-inline
+    :ensure t
+    :hook (flycheck-mode . turn-on-flycheck-inline)))
