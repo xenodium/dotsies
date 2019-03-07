53bcedc211214ee255456ab1f13526b507ab2056
xenodium <me+gh@xenodium.com>
2019-03-07 21:39:40 +0000

    Adding elisp-demos

--- emacs/features/help.el
+++ emacs/features/help.el
@@ -9,7 +9,14 @@
   :ensure t
   :bind  (("C-h f" . helpful-callable)
           ("C-h v" . helpful-variable)
-          ("C-h k" . helpful-key)))
+          ("C-h k" . helpful-key))
+  :config
+  (use-package elisp-demos
+    :ensure t
+    :config
+    (advice-add 'helpful-update
+                :after
+                #'elisp-demos-advice-helpful-update)))
 
 (use-package tldr
   :ensure
