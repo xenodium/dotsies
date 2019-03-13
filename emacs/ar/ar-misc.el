5e1bf0b1c3ae3a899ea4edc80b5507ebe85fcedb
xenodium <me+gh@xenodium.com>
2019-03-13 22:04:44 +0000

    Adding ar/misc-financial-times-lookup-symbol

--- emacs/ar/ar-misc.el
+++ emacs/ar/ar-misc.el
@@ -17,6 +17,11 @@
         (switch-to-buffer (find-file-noselect temp-file t))
       (error "Error: Could not create qrcode, check *qrencode* buffer"))))
 
+(defun ar/misc-financial-times-lookup-symbol ()
+  "Look up tearsheet for symbol at Financial Times."
+  (interactive)
+  (browse-url (format "https://markets.ft.com/data/funds/tearsheet/charts?s=%s" (read-string "Symbol: "))))
+
 (provide 'ar-misc)
 
 ;;; ar-misc.el ends here
