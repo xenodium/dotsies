2eb63627d63ae7c40a6a80d9ed936af8b25406a4
xenodium <me+gh@xenodium.com>
2019-03-16 17:58:31 +0000

    Disabling parts of pdf setup

--- emacs/features/pdf.el
+++ emacs/features/pdf.el
@@ -1,8 +1,8 @@
 (use-package pdf-tools
   :ensure t
-  :ensure-system-package poppler
+  ;; :ensure-system-package poppler
   :mode ("\\.pdf\\'" . pdf-view-mode)
   :config
-  (pdf-tools-install)
+  ;; (pdf-tools-install)
   (setq-default pdf-view-display-size 'fit-page)
   (setq pdf-annot-activate-created-annotations t))
