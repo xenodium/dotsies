af2ebb39343dded1f778c314845cae7f6b7c63a1
xenodium <me+gh@xenodium.com>
2019-03-19 07:34:58 +0000

    Adding ar/misc-diff-last-2-yanks

--- emacs/features/misc.el
+++ emacs/features/misc.el
@@ -1,3 +1,4 @@
 (use-package ar-misc
   :commands (ar/misc-clipboard-to-qr
-             ar/misc-financial-times-lookup-symbol))
+             ar/misc-financial-times-lookup-symbol
+             ar/misc-diff-last-2-yanks))
