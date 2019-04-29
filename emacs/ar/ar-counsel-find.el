04152a79f260a1dfd28b8018c06cec7a192f881b
xenodium <me+gh@xenodium.com>
2019-04-29 22:44:44 +0100

    Tweaking ar/counsel-find

--- emacs/ar/ar-counsel-find.el
+++ emacs/ar/ar-counsel-find.el
@@ -13,6 +13,8 @@
 
 (defvar ar/counsel-find--history nil "History for `ar/counsel-find'.")
 
+(defvar ar/counsel-find--last-command nil "Last find shell command.")
+
 (defun ar/counsel-find (arg)
   "Call the \"find\" shell command and fuzzy narrow using ivy. With ARG choose search path."
   (interactive "P")
@@ -24,33 +26,39 @@
 (defun ar/counsel--find-in-paths (paths)
   "Ivy narrow files found searching PATHS."
   (setq ar/counsel-find--paths paths)
-  (ivy-read "find: "
-            #'ar/counsel-find--function
-            :initial-input (when (use-region-p)
-                             (buffer-substring-no-properties (region-beginning)
-                                                             (region-end)))
-            :dynamic-collection t
-            :history 'ar/counsel-find--history
-            :action (lambda (file)
-                      (with-ivy-window
-                        (when file
-                          (find-file file))))
-            :unwind #'counsel-delete-process
-            :caller 'ar/counsel-find))
+  (let ((kmap (make-sparse-keymap)))
+    (define-key kmap (kbd "C-c C-e") (lambda ()
+                                       (interactive)
+                                       (ivy-quit-and-run
+                                         (message "Implement me"))))
+    (ivy-read "find: "
+              #'ar/counsel-find--function
+              :initial-input (when (use-region-p)
+                               (buffer-substring-no-properties (region-beginning)
+                                                               (region-end)))
+              :dynamic-collection t
+              :history 'ar/counsel-find--history
+              :action (lambda (file)
+                        (with-ivy-window
+                          (when file
+                            (find-file file))))
+              :unwind #'counsel-delete-process
+              :keymap kmap
+              :caller 'ar/counsel-find)))
 
 (defun ar/counsel-find--function (pattern)
   "Find files ivy function matching PATTERN."
-  (let* ((command (format "find %s -ipath '*%s*'"
-                          (s-join " " ar/counsel-find--paths)
-                          (s-replace-regexp "[ ]+" "*" pattern))))
-    (message "ar/counsel-find--function: %s" command)
-    (or
-     (ivy-more-chars)
-     (progn
-       (message "ar/counsel-find--function: %s" command)
-       (counsel--async-command command)
-       '("" "working...")))))
-
+  (setq ar/counsel-find--last-command
+        (format "find %s -ipath '*%s*'"
+                (s-join " " ar/counsel-find--paths)
+                (s-replace-regexp "[ ]+" "*" pattern)))
+  (message "ar/counsel-find--function: %s"
+           ar/counsel-find--last-command)
+  (or
+   (ivy-more-chars)
+   (progn
+     (counsel--async-command ar/counsel-find--last-command)
+     '("" "working..."))))
 
 (provide 'ar-counsel-find)
 
