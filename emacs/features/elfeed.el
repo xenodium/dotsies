f94c426c2e0b6d8017610d0bbb458e54a5023828
xenodium <me+gh@xenodium.com>
2019-05-04 17:15:30 +0100

    Updating

--- emacs/features/elfeed.el
+++ emacs/features/elfeed.el
@@ -148,6 +148,7 @@ Based on `elfeed-search-print-entry--default'."
               ("http://200ok.ch/atom.xml" blog emacs 200ok)
               ("http://akkartik.name/feeds.xml" blog dev)
               ("http://amitp.blogspot.com/feeds/posts/default" blog emacs AmitPatel)
+              ("http://beard.institute/index.xml" blog emacs BeardInstitute)
               ("http://ben-evans.com/benedictevans?format=RSS" blog dev Ben-Evans)
               ("http://blog.abhixec.com/index.xml" blog emacs RandomMusings)
               ("http://blog.davep.org/feed.xml" blog emacs davep)
