3873d86eefcc283ab46ce65b8d878dc0fbdded49
xenodium <me+gh@xenodium.com>
2019-05-12 20:38:41 +0100

    Trying out swift-format and sourcekit-lsp

--- emacs/features/fe-swift.el
+++ emacs/features/fe-swift.el
@@ -26,10 +26,21 @@
     (add-hook 'after-save-hook 'ar/after-swift-save nil t))
 
   (defun ar/after-swift-save ()
-    (call-process "swiftformat" nil "*swiftformat*" t "--indent" "2" buffer-file-name)
-    (call-process "swiftlint" nil "*swiftlint*" t "autocorrect"
-                  "--config" flycheck-swiftlint-config-file
-                  "--path" buffer-file-name))
+    (let ((swift-format-config-dpath (locate-dominating-file (buffer-file-name)
+                                                             ".swift-format.json")))
+      (if swift-format-config-dpath
+          (call-process "swift-format" nil "*swift-format*" t
+                        "--configuration" (expand-file-name (concat (file-name-as-directory
+                                                                     swift-format-config-dpath)
+                                                                    ".swift-format.json"))
+                        "-i"
+                        "-m" "format"
+                        buffer-file-name)
+        (message "No .swift-format.json found")))
+    ;; (call-process "swiftlint" nil "*swiftlint*" t "autocorrect"
+    ;;               "--config" flycheck-swiftlint-config-file
+    ;;               "--path" buffer-file-name)
+    )
   :config
   (require 'flycheck)
   (ar/vsetq swift-mode:basic-offset 2)
@@ -41,4 +52,10 @@
   (info-lookup-maybe-add-help
    :mode 'swift-mode
    :regexp "[#@_a-zA-Z][_a-zA-Z0-9]*"
-   :doc-spec '(("(swift)Index" nil "['`‘]" "['’]"))))
+   :doc-spec '(("(swift)Index" nil "['`‘]" "['’]")))
+
+  (use-package lsp-sourcekit
+    :config
+    (setenv "SOURCEKIT_TOOLCHAIN_PATH"
+            "/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain")
+    (setq lsp-sourcekit-executable (expand-file-name "~/local/bin/sourcekit-lsp"))))
