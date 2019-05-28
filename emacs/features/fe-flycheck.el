af0e65e0dff561c59d4fd7c4c8205a467ec40c47
xenodium <me+gh@xenodium.com>
2019-05-28 21:03:14 +0100

    Adding flycheck-status-emoji

--- emacs/features/fe-flycheck.el
+++ emacs/features/fe-flycheck.el
@@ -6,6 +6,15 @@
             '(save idle-change mode-enabled)
             flycheck-idle-change-delay 0.8)
 
+  (use-package flycheck-status-emoji
+    :ensure t
+    :custom
+    (flycheck-status-emoji-indicator-finished-error ?💀)
+    (flycheck-status-emoji-indicator-finished-ok ?👍)
+    (flycheck-status-emoji-indicator-finished-warning ?👎)
+    :config
+    (flycheck-status-emoji-mode +1))
+
   (use-package flycheck-inline
     :ensure t
     :hook (flycheck-mode . turn-on-flycheck-inline)))
