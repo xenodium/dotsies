6a89e98e90473994fcab4fcd4fdec5910128e3dc
xenodium <me+gh@xenodium.com>
2019-06-05 08:57:37 +0100

    Adding tags config

---
+++ emacs/features/fe-tags.el
@@ -0,0 +1,8 @@
+;; Needs .ctags and .globalrc in $HOME.
+(use-package counsel-gtags
+  :ensure t
+  :bind (:map
+         counsel-gtags-mode-map
+         ("M-." . counsel-gtags-dwim)
+         ("M-," . counsel-gtags-go-backward))
+  :hook ((swift-mode . counsel-gtags-mode)))
