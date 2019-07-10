f01ed3509cb2cf2dc2519608a0e6b32d84521c65
xenodium <me+gh@xenodium.com>
2019-07-10 22:03:30 +0100

    Updating ar/misc-financial-symbols to use (cons \"title\" \"symbol\")

--- emacs/ar/ar-misc.el
+++ emacs/ar/ar-misc.el
@@ -17,15 +17,18 @@
         (switch-to-buffer (find-file-noselect temp-file t))
       (error "Error: Could not create qrcode, check *qrencode* buffer"))))
 
-(defcustom ar/misc-financial-symbols nil "Default financial symbols to look up"
+(defcustom ar/misc-financial-symbols nil "Default financial symbols to look up (cons \"title\" \"symbol\")"
   :type 'list
   :group 'ar-misc)
 
 (defun ar/misc-financial-times-lookup-symbol ()
   "Look up tearsheet for symbol at Financial Times."
   (interactive)
-  (browse-url (format "https://markets.ft.com/data/funds/tearsheet/charts?s=%s"
-                      (completing-read "Symbol: " ar/misc-financial-symbols))))
+  (ivy-read "Symbol: " ar/misc-financial-symbols
+            :action (lambda (item)
+                      (assert (consp item) nil "List items must be a cons.")
+                      (browse-url (format "https://markets.ft.com/data/funds/tearsheet/charts?s=%s"
+                                          (cdr item))))))
 
 ;; From https://www.reddit.com/r/emacs/comments/b058f8/weekly_tipstricketc_thread/eilbynr
 (defun ar/misc-diff-last-2-yanks ()
