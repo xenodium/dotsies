1a41d5b1a60c1b18caa6cd3d0e14caee66687a6d
xenodium <me+gh@xenodium.com>
2019-08-29 20:57:02 +0100

    Making helpful navigation reuse current window

--- emacs/features/fe-help.el
+++ emacs/features/fe-help.el
@@ -1,4 +1,5 @@
 (require 'ar-vsetq)
+(require 'ar-csetq)
 
 (use-package help
   :config
@@ -16,7 +17,19 @@
     :config
     (advice-add 'helpful-update
                 :after
-                #'elisp-demos-advice-helpful-update)))
+                #'elisp-demos-advice-helpful-update))
+
+  (ar/csetq helpful-switch-buffer-function #'ar/helpful-switch-to-buffer)
+
+  ;; from https://d12frosted.io/posts/2019-06-26-emacs-helpful.html
+  (defun ar/helpful-switch-to-buffer (buffer-or-name)
+    "Switch to helpful BUFFER-OR-NAME.
+
+The logic is simple, if we are currently in the helpful buffer,
+reuse it's window, otherwise create new one."
+    (if (eq major-mode 'helpful-mode)
+        (switch-to-buffer buffer-or-name)
+      (pop-to-buffer buffer-or-name))))
 
 (use-package tldr
   :ensure
