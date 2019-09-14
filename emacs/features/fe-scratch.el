856e909a7698f1fddf53d77237625f0ab9a7cc8d
xenodium <me+gh@xenodium.com>
2019-09-14 12:56:35 +0100

    Workaround: make immortal-scratch play nice with persistent-scratch

--- emacs/features/fe-scratch.el
+++ emacs/features/fe-scratch.el
@@ -1,11 +1,35 @@
-;; Re-open scratch if killed.
-(use-package immortal-scratch
-  :ensure t
-  :config
-  (immortal-scratch-mode))
-
 ;; Remember scratch content across sessions.
 (use-package persistent-scratch
   :ensure t
   :config
   (persistent-scratch-setup-default))
+
+;; Re-open scratch if killed.
+(use-package immortal-scratch
+  :ensure t
+  :after persistent-scratch
+  :config
+  (defun ar/immortal-scratch-respawn-advice (orig-fun &rest r)
+    "Advice function around `immortal-scratch-respawn-advice' (ORIG-FUN and R)."
+    (let ((initial-scratch-message
+           ;; `persistent-scratch-save-file' content looks as follows:
+           ;; (["*scratch*" "This is the actual content" (24) fundamental-mode nil nil])
+           ;; Need to unpack actual content  with nth and aref.
+           (aref
+            (nth 0
+                 (read
+                  (with-temp-buffer
+                    (let ((coding-system-for-read 'utf-8-unix))
+                      (insert-file-contents persistent-scratch-save-file))
+                    (buffer-string)))) 1)))
+      ;; Now that `initial-scratch-message' is momentarily set,
+      ;; invoke `immortal-scratch-respawn-advice'
+      (apply orig-fun r)))
+
+  ;; If `initial-scratch-message' is nil, immortal-scratch-respawn breaks.
+  ;; Temporarily let-set `initial-scratch-message' in adviced function
+  ;; using persistent-scratch's value.
+  (advice-add #'immortal-scratch-respawn
+              :around
+              'ar/immortal-scratch-respawn-advice)
+  (immortal-scratch-mode))
