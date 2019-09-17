aa78a95c37afae9286fe90adbf6744238aa9316d
xenodium <me+gh@xenodium.com>
2019-09-17 11:07:37 +0100

    Fixing typo

--- emacs/features/fe-scratch.el
+++ emacs/features/fe-scratch.el
@@ -11,7 +11,7 @@
   :custom (immortal-scratch-switch-to-respawned-scratch t)
   :config
   (defun ar/immortal-scratch-respawn-advice (orig-fun &rest r)
-    "Advice function around `immortal-scratch-respawn-advice' (ORIG-FUN and R)."
+    "Advice function around `immortal-scratch-respawn' (ORIG-FUN and R)."
     (let ((initial-scratch-message
            ;; `persistent-scratch-save-file' content looks as follows:
            ;; (["*scratch*" "This is the actual content" (24) fundamental-mode nil nil])
@@ -24,7 +24,7 @@
                       (insert-file-contents persistent-scratch-save-file))
                     (buffer-string)))) 1)))
       ;; Now that `initial-scratch-message' is momentarily set,
-      ;; invoke `immortal-scratch-respawn-advice'
+      ;; invoke `immortal-scratch-respawn'
       (apply orig-fun r)
       ;; Scroll to top.
       (with-current-buffer (get-buffer-create "*scratch*")
