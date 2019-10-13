17b413d24de9a3acc1235a6ba62ae18421203eb0
xenodium <me+gh@xenodium.com>
2019-10-13 21:04:59 +0100

    Fixing ar/swiper-isearch-*

--- emacs/features/fe-ivy.el
+++ emacs/features/fe-ivy.el
@@ -166,7 +166,7 @@ For example:
   :config
   (defun ar/swiper-isearch-backward-dwim ()
     (interactive)
-    (if (and (boundp 'multiple-cursors-mode)
+    (if (and (fboundp 'multiple-cursors-mode)
              multiple-cursors-mode)
         (call-interactively 'isearch-backward)
       (let ((ivy-wrap t))
@@ -178,7 +178,7 @@ For example:
 
   (defun ar/swiper-isearch-dwim ()
     (interactive)
-    (if (and (boundp 'multiple-cursors-mode)
+    (if (and (fboundp 'multiple-cursors-mode)
              multiple-cursors-mode)
         (call-interactively 'isearch-forward)
       (let ((ivy-wrap t))
