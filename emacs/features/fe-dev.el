087302035b6ac1e7f62c2e67289730dfc8612c2e
xenodium <me+gh@xenodium.com>
2019-10-20 23:07:02 +0100

    Fixing ar/comment-dwim

--- emacs/features/fe-dev.el
+++ emacs/features/fe-dev.el
@@ -5,14 +5,14 @@
   (let ((start (line-beginning-position))
         (end (line-end-position)))
     (when (region-active-p)
-      (ar/vsetq start (save-excursion
-                             (goto-char (region-beginning))
-                             (beginning-of-line)
-                             (point))
-                     end (save-excursion
-                           (goto-char (region-end))
-                           (end-of-line)
-                           (point))))
+      (setq start (save-excursion
+                    (goto-char (region-beginning))
+                    (beginning-of-line)
+                    (point))
+            end (save-excursion
+                  (goto-char (region-end))
+                  (end-of-line)
+                  (point))))
     (comment-or-uncomment-region start end)))
 
 (bind-key "M-;" #'ar/comment-dwim)
