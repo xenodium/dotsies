156e28db3bb0cfb4f37b347e688a01fd2071e793
xenodium <me+gh@xenodium.com>
2019-10-28 23:42:17 +0000

    Use find-file-visit-truename and migrating fe-files.el to :custom

--- emacs/features/fe-files.el
+++ emacs/features/fe-files.el
@@ -1,25 +1,23 @@
 ;;; -*- lexical-binding: t; -*-
-(require 'ar-vsetq)
-(require 'ar-csetq)
-
 (use-package files
-  :config
-  (require 'ar-platform)
-  ;; Disable backup.
+  :custom
+  ;; Always display opened file using canonical location (not symlink).
+  (find-file-visit-truename t)
   ;; From: http://anirudhsasikumar.net/blog/2005.01.21.html
-  (ar/vsetq backup-inhibited t)
+  (backup-inhibited t)
+  ;; Disable backup.
   ;; Ensure files end with newline.
-  ;; (ar/csetq require-final-newline t)
+  ;; (require-final-newline t)
   ;; Disable auto save.
   ;; From: http://anirudhsasikumar.net/blog/2005.01.21.html
-  (ar/csetq auto-save-default nil)
-
+  (auto-save-default nil)
   ;; I've inadvertedly exited Emacs far too many times.
   ;; Ask for confirmation.
-  (ar/csetq confirm-kill-emacs 'yes-or-no-p)
-
+  (confirm-kill-emacs 'yes-or-no-p)
   ;; Open that large file! YOLO. Ok, got `openwith' to handle it.
-  (ar/csetq large-file-warning-threshold nil)
+  (large-file-warning-threshold nil)
+  :config
+  (require 'ar-platform)
 
   (defun ar/open-clipboard-file-externally ()
     (interactive)
@@ -30,36 +28,36 @@
     (find-file (current-kill 0))))
 
 (use-package autorevert
-  :config
+  :custom
   ;; Auto refresh dired.
   ;; https://mixandgo.com/learn/how-ive-convinced-emacs-to-dance-with-ruby
-  (ar/csetq global-auto-revert-non-file-buffers t)
+  (global-auto-revert-non-file-buffers t)
 
   ;; Be quiet about dired refresh.
-  (ar/csetq auto-revert-verbose nil)
-
+  (auto-revert-verbose nil)
+  :config
   (global-auto-revert-mode))
 
 ;; Avoid creating lock files (ie. .#some-file.el)
 (setq create-lockfiles nil)
 
 (use-package recentf
+  :custom
+  (recentf-exclude '("/auto-install/"
+                     ".recentf"
+                     "/repos/"
+                     "/elpa/"
+                     "\\.mime-example"
+                     "\\.ido.last"
+                     "COMMIT_EDITMSG"
+                     ".gz"
+                     "~$"
+                     "/ssh:"
+                     "/sudo:"
+                     "/scp:"))
+  (recentf-max-saved-items 1000
+                           recentf-max-menu-items 50)
   :config
-  (ar/vsetq recentf-exclude '("/auto-install/"
-                              ".recentf"
-                              "/repos/"
-                              "/elpa/"
-                              "\\.mime-example"
-                              "\\.ido.last"
-                              "COMMIT_EDITMSG"
-                              ".gz"
-                              "~$"
-                              "/ssh:"
-                              "/sudo:"
-                              "/scp:"))
-  (ar/vsetq recentf-max-saved-items 1000
-            recentf-max-menu-items 50)
-
   (use-package recentf-ext
     :ensure t)
 
