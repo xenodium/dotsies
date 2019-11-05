eb4aca89ca371c5e0626aabcd406116981c620ce
xenodium <me+gh@xenodium.com>
2019-11-05 21:32:26 +0000

    Replacing immortal-scratch with emacs-lock-mode

--- emacs/features/fe-scratch.el
+++ emacs/features/fe-scratch.el
@@ -3,39 +3,10 @@
 (use-package persistent-scratch
   :ensure t
   :config
-  (persistent-scratch-setup-default))
-
-;; Re-open scratch if killed.
-(use-package immortal-scratch
-  :ensure t
-  :after persistent-scratch
-  :custom
-  (immortal-scratch-switch-to-respawned-scratch t)
-  :config
-  (defun ar/immortal-scratch-respawn-advice (orig-fun &rest r)
-    "Advice function around `immortal-scratch-respawn' (ORIG-FUN and R)."
-    (let ((initial-scratch-message
-           ;; `persistent-scratch-save-file' content looks as follows:
-           ;; (["*scratch*" "This is the actual content" (24) fundamental-mode nil nil])
-           ;; Need to unpack actual content  with nth and aref.
-           (aref
-            (nth 0
-                 (read
-                  (with-temp-buffer
-                    (let ((coding-system-for-read 'utf-8-unix))
-                      (insert-file-contents persistent-scratch-save-file))
-                    (buffer-string)))) 1)))
-      ;; Now that `initial-scratch-message' is momentarily set,
-      ;; invoke `immortal-scratch-respawn'
-      (apply orig-fun r)
-      ;; Scroll to top.
-      (with-current-buffer (get-buffer-create "*scratch*")
-        (goto-char (point-min)))))
-
-  ;; If `initial-scratch-message' is nil, immortal-scratch-respawn breaks.
-  ;; Temporarily let-set `initial-scratch-message' in adviced function
-  ;; using persistent-scratch's value.
-  (advice-add #'immortal-scratch-respawn
-              :around
-              'ar/immortal-scratch-respawn-advice)
-  (immortal-scratch-mode))
+  (with-current-buffer "*scratch*"
+    (emacs-lock-mode 'kill))
+  ;; Show persistent scratch ASAP.
+  (persistent-scratch-setup-default)
+  ;; Reload emacs-lisp-mode in a few seconds.
+  ;; (with all the goodies from lazy-loaded packages)
+  (run-with-timer 3 nil #'emacs-lisp-mode))
