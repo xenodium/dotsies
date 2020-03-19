10d30575bc115599f8ab2930d6d377fa4009b26b
xenodium <me+gh@xenodium.com>
2020-03-19 11:50:53 +0000

    Binding paradox-menu-visit-homepage to "b" like other packages

--- emacs/features/fe-paradox.el
+++ emacs/features/fe-paradox.el
@@ -2,5 +2,7 @@
 (use-package paradox
   :ensure t
   :commands (paradox-list-packages)
+  :bind  (:map paradox-menu-mode-map
+               ("b" . paradox-menu-visit-homepage))
   :config
   (fullframe paradox-list-packages paradox-quit-and-close))
