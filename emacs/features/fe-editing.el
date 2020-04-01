50edb3512f54386b49082eaf210ffa02bcf97525
xenodium <me+gh@xenodium.com>
2020-04-01 22:02:27 +0100

    Ensure ar/whitespace-mode-enable is not inadvertently in wrong buffer

--- emacs/features/fe-editing.el
+++ emacs/features/fe-editing.el
@@ -98,9 +98,11 @@
   (defun ar/whitespace-mode-enable ()
     "Delayed enabling of whitespace-mode to ensure fill-column is set for loaded buffer."
     (whitespace-mode -1)
-    (run-with-timer 1 nil
-                    (lambda ()
-                      (whitespace-mode +1))))
+    (let ((buffer (current-buffer)))
+      (run-with-timer 1 nil
+                      (lambda ()
+                        (with-current-buffer buffer
+                          (whitespace-mode +1))))))
   ;; When nil, fill-column is used instead.
   (ar/vsetq whitespace-line-column nil)
   ;; Highlight empty lines, TABs, blanks at beginning/end, lines
