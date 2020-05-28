a273616c39b4ee514616b43f0e7905277744740e
xenodium <me+gh@xenodium.com>
2020-05-28 10:26:14 +0100

    Fixing search in pdf-tools (swiper doesn't work here)

--- emacs/features/fe-pdf.el
+++ emacs/features/fe-pdf.el
@@ -3,6 +3,9 @@
   :ensure t
   ;; :ensure-system-package poppler
   :mode ("\\.pdf\\'" . pdf-view-mode)
+  :bind (:map pdf-view-mode-map
+              ;; Swiper doesn't work in pdf-tools.
+              ("C-s" . 'isearch-forward))
   :config
   ;; (pdf-tools-install)
   (setq-default pdf-view-display-size 'fit-page)
