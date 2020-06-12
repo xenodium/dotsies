114a803769a65c1a4c7bde840969b11a6dca1993
xenodium <me+gh@xenodium.com>
2020-06-12 18:45:15 +0100

    Add fallback counsel-etags search

--- emacs/features/fe-tags.el
+++ emacs/features/fe-tags.el
@@ -35,7 +35,8 @@
   :config
   (defun counsel-etags-fallback-rg-search (&optional default-keyword prompt root)
     "A non-blocking alternative to counsel-etags-grep."
-    (assert (counsel-etags-has-quick-grep) "ripgrep command-line utility not found")
+    (unless (counsel-etags-has-quick-grep)
+      (error "ripgrep command-line utility not found"))
     (let ((text (if default-keyword default-keyword ""))
           (default-directory (file-truename (or root
                                                 (counsel-etags-locate-project))))
@@ -50,6 +51,19 @@
 
       (counsel-rg text default-directory options prompt)))
 
+  (defvar ar/counsel-etags-last-tag-searched "")
+
+  (defun adviced:counsel-etags-open-tag-cand (orig-fun &rest r)
+    "Additional support for multiple cursors."
+    (let ((tagname (nth 0 r)))
+      (setq ar/counsel-etags-last-tag-searched tagname)
+      (setq )
+      (apply orig-fun r)))
+
+  (advice-add #'counsel-etags-open-tag-cand
+              :around
+              #'adviced:counsel-etags-open-tag-cand)
+
   (defun adviced:counsel-etags-grep (orig-fun &rest r)
     "Additional support for multiple cursors."
     (let ((default-keyword (nth 0 r))
@@ -59,4 +73,33 @@
 
   (advice-add #'counsel-etags-grep
               :around
-              #'adviced:counsel-etags-grep))
+              #'adviced:counsel-etags-grep)
+
+  (defun ar/counsel-etags-search-action (item)
+    (counsel-etags-fallback-rg-search ar/counsel-etags-last-tag-searched
+                                      "Search: "
+                                      (or (counsel-etags-tags-file-directory)
+                                          (error "No TAGS found"))))
+
+  (ivy-add-actions
+   'counsel-etags-find-tag '(("s" ar/counsel-etags-search-action "fallback search"))))
+
+
+;; --langdef=swift
+;; --langmap=swift:+.swift
+
+;; --kinddef-swift=v,variable,variables
+;; --kinddef-swift=f,function,functions
+;; --kinddef-swift=s,struct,structs
+;; --kinddef-swift=c,class,classes
+;; --kinddef-swift=p,protocol,protocols
+;; --kinddef-swift=e,enum,enums
+;; --kinddef-swift=t,typealias,typealiases
+
+;; --regex-swift=/(var|let)[ \t]+([^:=]+).*$/\2/v/
+;; --regex-swift=/func[ \t]+([^\(\)]+)\([^\(\)]*\)/\1/f/
+;; --regex-swift=/struct[ \t]+([^:\{]+).*$/\1/s/
+;; --regex-swift=/class[ \t]+([^:\{]+).*$/\1/c/
+;; --regex-swift=/protocol[ \t]+([^:\{]+).*$/\1/p/
+;; --regex-swift=/enum[ \t]+([^:\{]+).*$/\1/e/
+;; --regex-swift=/(typealias)[ \t]+([^:=]+).*$/\2/v/
