b0fcfda231efe5f3ca6b748cc55a5e22a57afdbc
xenodium <me+gh@xenodium.com>
2020-06-29 15:58:15 +0100

    Removing unused function

--- emacs/features/fe-protobuf.el
+++ emacs/features/fe-protobuf.el
@@ -9,16 +9,4 @@
     (setq-local imenu-generic-expression
                 '((nil "^[[:space:]]*message[[:space:]]+[[:alpha:]][[:alnum:]_]+" 0)
                   (nil "^[[:space:]]*enum[[:space:]]+[[:alpha:]][[:alnum:]_]+" 0)
-                  (nil "^[[:space:]]*service[[:space:]]+[[:alpha:]][[:alnum:]_]+" 0))))
-
-  (defun ar/reindex-proto-fields ()
-    "From within a proto message, reindex all proto field tags."
-    (interactive)
-    (save-excursion
-      (save-restriction
-        (narrow-to-defun)
-        (goto-char (point-min))
-        (let ((counter 1))
-          (while (search-forward-regexp "\\(\\(\\(optional\\)\\|\\(required\\)\\).*= *\\)[1-9]+" nil t)
-            (replace-match (format "\\1%d" counter) t nil)
-            (setq counter (+ counter 1))))))))
+                  (nil "^[[:space:]]*service[[:space:]]+[[:alpha:]][[:alnum:]_]+" 0)))))
