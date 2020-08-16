4630a01f01c81827d6ff56c9ff9de94a2ea8e528
xenodium <me+gh@xenodium.com>
2020-08-16 22:41:32 +0100

    Turning off flycheck and flyspell

--- emacs/features/fe-prog.el
+++ emacs/features/fe-prog.el
@@ -13,16 +13,18 @@
          prog-mode-map
          ("C-x C-q" . view-mode))
   :hook ((prog-mode . company-mode)
-         (prog-mode . flycheck-mode)
-         (prog-mode . flyspell-prog-mode)
+;; Trying without (for performance).
+;;         (prog-mode . flycheck-mode)
+;;         (prog-mode . flyspell-prog-mode)
          (prog-mode . yas-minor-mode)
          ;; Trying to go without.
          ;; (prog-mode . centered-cursor-mode)
          (prog-mode . rainbow-mode)
          (prog-mode . goto-address-prog-mode))
   :config
-  (require 'flyspell)
-  (require 'flycheck)
+;; Trying without (for performance).
+;;  (require 'flyspell)
+;;  (require 'flycheck)
 
   ;; Highlight hex strings in respective color.
   (use-package rainbow-mode
