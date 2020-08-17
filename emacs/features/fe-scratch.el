f9bf62ca368202811eb3c5ea8058961fa8e9b867
xenodium <me+gh@xenodium.com>
2020-08-17 21:56:07 +0100

    Fix delayed scratch

--- emacs/features/fe-scratch.el
+++ emacs/features/fe-scratch.el
@@ -10,7 +10,7 @@
   ;; Reload emacs-lisp-mode (with all the goodies from lazy-loaded packages)
   (run-with-timer 60 nil (lambda ()
                            (with-current-buffer "*scratch*"
-			     (emacs-lisp-mode +1)))))
+			     (emacs-lisp-mode)))))
 
 ;; Lock scratch buffer (can't be killed).
 (run-with-timer 5 nil (lambda ()
