2b6776da549596e95bdd11be5541417e7ccf6d10
xenodium <me+gh@xenodium.com>
2020-08-18 19:50:36 +0100

    Fixing modal

--- emacs/features/fe-modal.el
+++ emacs/features/fe-modal.el
@@ -2,6 +2,7 @@
 
 ;; Search for things modally, using ivy.
 (use-package modal-ivy
+  ;; Need these accessible outside Emacs.
   :commands (ar/modal-ivy-search-org-links
              ar/modal-ivy-search-short-links
              ar/modal-ivy-clipboard
@@ -12,9 +13,7 @@
   :config
   ;; Personal modal ivy extensions.
   (use-package arp-modal-ivy
-    :if (locate-library "arp-modal-ivy")
-    :defer 5)
+    :if (locate-library "arp-modal-ivy"))
 
   ;; Add TODOs modally.
-  (use-package ar-hammerspoon-org-modal
-    :defer 5))
+  (use-package ar-hammerspoon-org-modal))
