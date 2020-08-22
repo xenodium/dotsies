1bed013db6c4a284693e79cf43b0bab8906565f2
xenodium <me+gh@xenodium.com>
2020-08-22 17:22:55 +0100

    Cleanup

--- emacs/features/fe-java.el
+++ emacs/features/fe-java.el
@@ -1,8 +1,8 @@
 ;;; -*- lexical-binding: t; -*-
 
 (use-package kotlin-mode
+  :ensure t
   :mode ("\\.kts?\\'" . kotlin-mode)
   :bind (:map
          kotlin-mode-map
-         ("C-c C-c" . ar/compile))
-  :ensure t)
+         ("C-c C-c" . ar/compile)))
