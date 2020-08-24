826f86c643e8e755b711929a3c488895b36cb76c
xenodium <me+gh@xenodium.com>
2020-08-24 23:09:51 +0100

    Make Swift init yasnippet smarter

---
+++ emacs/yasnippets/personal/swift-mode/.yas-setup.el
@@ -0,0 +1,29 @@
+;;; -*- lexical-binding: t; -*-
+
+;; Swift helpers adjusted from:
+;; https://github.com/jorgenschaefer/elpy/blob/060a4eb78ec8eba9c8fe3466c40a414d84b3dc81/snippets/python-mode/.yas-setup.el
+
+(defun swift-snippet-init-assignments (arg-string)
+  "Return init assignments for arguments."
+  (let ((indentation (make-string (save-excursion
+                                    (goto-char start-point)
+                                    (current-indentation))
+                                  ?\s)))
+    (mapconcat (lambda (arg)
+                 (if (string-match "^\\*" (car arg))
+                     ""
+                   (format "self.%s = %s\n%s"
+                           (car arg)
+                           (car arg)
+                           indentation)))
+               (swift-snippet-split-args arg-string)
+               "")))
+
+(defun swift-snippet-split-args (arg-string)
+  "Split a python argument string into ((name, default)..) tuples"
+  (mapcar (lambda (x)
+            (list
+             (if (and x (string-match "\\([[:alnum:]]*\\):" x))
+                 (match-string-no-properties 1 x)
+               x)))
+          (split-string arg-string "[[:blank:]]*,[[:blank:]]*" t)))
