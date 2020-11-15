4d3fac36ebb729eb8a31df59ea164dcf311a4813
xenodium <me+gh@xenodium.com>
2020-11-15 15:54:07 +0000

    Adding feed

--- emacs/features/fe-elfeed.el
+++ emacs/features/fe-elfeed.el
@@ -202,6 +202,7 @@ Based on `elfeed-search-print-entry--default'."
 
   (setq elfeed-feeds '(
                        ("http://1w6.org/rss.xml" blog emacs draketo)
+                       ("https://feeds.feedburner.com/JakeMccrarysMusings" blog emacs JakeMccrary)
                        ("http://www.nextpoint.se/?feed=comments-rss2" blog emacs nextpoint)
                        ("http://200ok.ch/atom.xml" blog emacs 200ok)
                        ("http://akkartik.name/feeds.xml" blog dev)
