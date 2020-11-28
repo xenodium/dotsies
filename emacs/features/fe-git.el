b47ff27c8f4b28de8ef78471455f3be76a7948d1
xenodium <me+gh@xenodium.com>
2020-11-28 23:27:48 +0000

    Adding `ar/git-clone-clipboard-url`

--- emacs/features/fe-git.el
+++ emacs/features/fe-git.el
@@ -141,3 +141,36 @@ on the current line, if any."
   :defer
   :validate-custom
   (diff-font-lock-prettify t))
+
+(defun ar/git-clone-clipboard-url ()
+  "Clone git URL in clipboard asynchronously and open in dired when finished."
+  (interactive)
+  (cl-assert (string-match-p "^http" (current-kill 0)) nil "No URL in clipboard")
+  (let* ((url (current-kill 0))
+         (download-dir (expand-file-name "~/Downloads/"))
+         (project-dir (concat (file-name-as-directory download-dir)
+                              (file-name-base url)))
+         (default-directory download-dir)
+         (command (format "git clone %s" url))
+         (buffer (generate-new-buffer (format "*%s*" command))))
+    (when (file-exists-p project-dir)
+      (if (y-or-n-p (format "%s exists. delete?" (file-name-base url)))
+          (delete-directory project-dir t)
+        (user-error "Bailed")))
+    (switch-to-buffer buffer)
+    (setq proc (start-process-shell-command (nth 0 (split-string command)) buffer command))
+    (with-current-buffer buffer
+      (setq default-directory download-dir)
+      (shell-command-save-pos-or-erase)
+      (require 'shell)
+      (shell-mode))
+    (set-process-sentinel proc (lambda (process state)
+                                 (if (= (process-exit-status process) 0)
+                                     (progn
+                                       (message "%s ✔" command)
+                                       (dired project-dir))
+                                   (message "%s ❌" command)
+                                   (message (with-current-buffer (process-buffer process)
+                                              (buffer-string))))
+                                 (kill-buffer (process-buffer process))))
+    (set-process-filter proc #'comint-output-filter)))
