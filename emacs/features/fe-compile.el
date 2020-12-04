fca04d09411b32f96956f7dffb81767d90086258
xenodium <me+gh@xenodium.com>
2020-12-04 16:55:29 +0000

    Cleaning up ar/compile

--- emacs/features/fe-compile.el
+++ emacs/features/fe-compile.el
@@ -23,21 +23,6 @@
          prog-mode-map
          ("C-c C-c" . ar/compile))
   :config
-
-  (defun ar/compile (pfx)
-    """Run the same compile as the last time.
-
-If there was no last time, or there is a prefix argument, this acts like
-M-x compile.
-"""
-    (interactive "p")
-    (if (and (eq pfx 1)
-	     compilation-last-buffer)
-        (progn
-          (set-buffer compilation-last-buffer)
-          (revert-buffer t t))
-      (call-interactively 'compile)))
-
   (defun ar/compile--history-path ()
     (concat (file-name-as-directory (expand-file-name "~/.emacs.d/")) ".comphist.el"))
 
@@ -102,19 +87,6 @@ M-x compile.
       (makunbound 'ar/compile--directory)
       (makunbound 'ar/compile--project-root)))
 
-  ;; http://ivanmalison.github.io/dotfiles/#colorizecompliationbuffers
-  (defun ar/colorize-compilation-buffer ()
-    (let ((was-read-only buffer-read-only))
-      (unwind-protect
-          (progn
-            (when was-read-only
-              (read-only-mode -1))
-            (ansi-color-apply-on-region (point-min) (point-max)))
-        (when was-read-only
-          (read-only-mode +1)))))
-
-  (add-hook 'compilation-filter-hook 'ar/colorize-compilation-buffer)
-
   (defun ar/compile-autoclose (buffer string)
     "Hide successful builds window with BUFFER and STRING."
     (cond ((string-match "finished" string)
@@ -129,4 +101,17 @@ M-x compile.
 
   ;; Automatically hide successful builds window.
   ;; Trying out without for a little while.
-  (setq compilation-finish-functions (list #'ar/compile-autoclose #'ar/compile-cache-env)))
+  (setq compilation-finish-functions (list #'ar/compile-autoclose #'ar/compile-cache-env))
+
+  ;; http://ivanmalison.github.io/dotfiles/#colorizecompliationbuffers
+  (defun ar/colorize-compilation-buffer ()
+    (let ((was-read-only buffer-read-only))
+      (unwind-protect
+          (progn
+            (when was-read-only
+              (read-only-mode -1))
+            (ansi-color-apply-on-region (point-min) (point-max)))
+        (when was-read-only
+          (read-only-mode +1)))))
+
+  (add-hook 'compilation-filter-hook 'ar/colorize-compilation-buffer))
