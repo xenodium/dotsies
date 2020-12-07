17a9a262c2e998e2510963961850288779a9dfcd
xenodium <me+gh@xenodium.com>
2020-12-07 08:40:09 +0000

    Moving macOS specific commands to fe-mac

--- emacs/features/fe-ivy.el
+++ emacs/features/fe-ivy.el
@@ -303,42 +303,6 @@ With prefix argument, use full path."
 	  (insert (if full-path filename (file-name-nondirectory filename)))
         (error (format "Buffer %s is not visiting a file" (buffer-name buffer))))))
 
-  (defun ar/ivy-bluetooth-connect ()
-    "Connect to paired bluetooth device."
-    (interactive)
-    (cl-assert (string-equal system-type "darwin")
-            nil "macOS only. Sorry :/")
-    (cl-assert (executable-find "BluetoothConnector")
-            nil "Install BluetoothConnector from https://github.com/lapfelix/BluetoothConnector")
-    (ivy-read "(Dis)connect: "
-              (seq-map
-               (lambda (item)
-                 (let* ((device (split-string item " - "))
-                        (mac (nth 0 device))
-                        (name (nth 1 device)))
-                   (propertize name
-                               'mac mac)))
-               (seq-filter
-                (lambda (line)
-                  ;; Keep lines like: af-8c-3b-b1-99-af - Device name
-                  (string-match-p "^[0-9a-f]\\{2\\}" line))
-                (with-current-buffer (get-buffer-create "*BluetoothConnector*")
-                  (erase-buffer)
-                  ;; BluetoothConnector exits with 64 if no param is given.
-                  ;; Invoke with no params to get a list of devices.
-                  (unless (eq 64 (call-process "BluetoothConnector" nil (current-buffer)))
-                    (error (buffer-string)))
-                  (split-string (buffer-string) "\n"))))
-              :require-match t
-              :preselect (when (boundp 'ar/misc-bluetooth-connect--history)
-                           (nth 0 ar/misc-bluetooth-connect--history))
-              :history 'ar/misc-bluetooth-connect--history
-              :caller 'ar/toggle-bluetooth-connection
-              :action (lambda (device)
-                        (start-process "BluetoothConnector"
-                                       (get-buffer-create "*BluetoothConnector*")
-                                       "BluetoothConnector" (get-text-property 0 'mac device) "--notify"))))
-
   (defun ar/ivy-keyboard-quit-dwim ()
     "If region active, deactivate. If there's content, minibuffer. Otherwise quit."
     (interactive)
--- emacs/features/fe-mac.el
+++ emacs/features/fe-mac.el
@@ -48,4 +48,56 @@
            ("C-c m n" . musica-play-next)
            ("C-c m p" . musica-play-previous)
            ("C-c m SPC" . musica-play-pause)
-           ("C-c m r" . musica-play-next-random))))
+           ("C-c m r" . musica-play-next-random)))
+
+  (defun ar/ivy-bluetooth-connect ()
+    "Connect to paired bluetooth device."
+    (interactive)
+    (require 'cl-lib)
+    (require 'ivy)
+    (cl-assert (string-equal system-type "darwin")
+               nil "macOS only. Sorry :/")
+    (cl-assert (executable-find "BluetoothConnector")
+               nil "Install BluetoothConnector from https://github.com/lapfelix/BluetoothConnector")
+    (ivy-read "(Dis)connect: "
+              (seq-map
+               (lambda (item)
+                 (let* ((device (split-string item " - "))
+                        (mac (nth 0 device))
+                        (name (nth 1 device)))
+                   (propertize name
+                               'mac mac)))
+               (seq-filter
+                (lambda (line)
+                  ;; Keep lines like: af-8c-3b-b1-99-af - Device name
+                  (string-match-p "^[0-9a-f]\\{2\\}" line))
+                (with-current-buffer (get-buffer-create "*BluetoothConnector*")
+                  (erase-buffer)
+                  ;; BluetoothConnector exits with 64 if no param is given.
+                  ;; Invoke with no params to get a list of devices.
+                  (unless (eq 64 (call-process "BluetoothConnector" nil (current-buffer)))
+                    (error (buffer-string)))
+                  (split-string (buffer-string) "\n"))))
+              :require-match t
+              :preselect (when (boundp 'ar/misc-bluetooth-connect--history)
+                           (nth 0 ar/misc-bluetooth-connect--history))
+              :history 'ar/misc-bluetooth-connect--history
+              :caller 'ar/toggle-bluetooth-connection
+              :action (lambda (device)
+                        (start-process "BluetoothConnector"
+                                       (get-buffer-create "*BluetoothConnector*")
+                                       "BluetoothConnector" (get-text-property 0 'mac device) "--notify"))))
+
+  (defun ar/display-toggle-rotation ()
+    (interactive)
+    (require 'cl-lib)
+    (cl-assert (executable-find "fb-rotate") nil
+               "Install fb-rotate from https://github.com/CdLbB/fb-rotate")
+    ;; #  Display_ID    Resolution  ____Display_Bounds____  Rotation
+    ;; 2  0x2b347692    1440x2560      0     0  1440  2560    270    [main]
+    ;; From fb-rotate output, get the `current-rotation' from Column 7, row 1 zero-based.
+    (let ((current-rotation (nth 7 (split-string (nth 1 (process-lines "fb-rotate" "-i"))))))
+      (call-process-shell-command (format "fb-rotate -d 1 -r %s"
+                                          (if (equal current-rotation "270")
+                                              "0"
+                                            "270"))))))
