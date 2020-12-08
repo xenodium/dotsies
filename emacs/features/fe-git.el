80bd67e8226c9b5ba57aa8b22b7d71094113006c
xenodium <me+gh@xenodium.com>
2020-12-08 21:03:36 +0000

    Swapping git-gutter for diff-hl
    https://emacsredux.com/blog/2020/12/08/favorite-emacs-packages

--- emacs/features/fe-git.el
+++ emacs/features/fe-git.el
@@ -101,25 +101,14 @@ on the current line, if any."
     ;; Remember more commit messages.
     (log-edit-comment-ring (make-ring 1000))))
 
-(use-package git-gutter
+(use-package diff-hl
   :ensure t
-  :hook ((prog-mode . ar/delayed-git-gutter-mode)
-         (protobuf-mode . git-gutter-mode))
-  :bind (("C-c <up>" . git-gutter:previous-hunk)
-         ("C-c <down>" . git-gutter:next-hunk))
-  :validate-custom
-  (git-gutter:handled-backends '(git hg))
+  :hook ((prog-mode . diff-hl-mode)
+         (protobuf-mode . diff-hl-mode))
+  :bind (("C-c <up>" . diff-hl-previous-hunk)
+         ("C-c <down>" . diff-hl-next-hunk))
   :config
-  (defun ar/delayed-git-gutter-mode ()
-    "Git gutter can take time to load on some repos. Delay enabling."
-    (if (file-remote-p default-directory)
-        (message "Ignoring git-gutter (tramp): %s" buffer-file-name)
-      (let ((buffer (current-buffer)))
-        (run-with-idle-timer 3 nil
-                             (lambda ()
-                               (when (buffer-live-p buffer)
-                                 (with-current-buffer buffer
-                                   (git-gutter-mode +1)))))))))
+  (add-hook 'magit-pre-refresh-hook 'diff-hl-magit-pre-refresh))
 
 (use-package gitconfig-mode
   :ensure t
