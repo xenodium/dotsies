c3f5faa9ad73e523496b35985e3902a65df00274
xenodium <me+gh@xenodium.com>
2020-12-31 18:07:09 +0000

    Fixing fe-modal loading

--- emacs/features/fe-modal.el
+++ emacs/features/fe-modal.el
@@ -6,14 +6,7 @@
   :commands (ar/modal-ivy-search-org-links
              ar/modal-ivy-search-short-links
              ar/modal-ivy-clipboard
-             ar/modal-ivy-search-short-links
-             ar/modal-key-binding-r
-             ar/hammerspoon-org-modal-add-todo
-             ar/modal-key-binding-i)
+             ar/hammerspoon-org-modal-add-todo)
   :config
-  ;; Personal modal ivy extensions.
-  (use-package arp-modal-ivy
-    :if (locate-library "arp-modal-ivy"))
-
   ;; Add TODOs modally.
   (use-package ar-hammerspoon-org-modal))
