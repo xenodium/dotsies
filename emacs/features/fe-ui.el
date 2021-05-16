f556c0eef10f9796e448fb105f0506cac0f3be9f
xenodium <me+gh@xenodium.com>
2021-05-16 19:25:27 +0100

    Trying out JetBrains Mono font

--- emacs/features/fe-ui.el
+++ emacs/features/fe-ui.el
@@ -41,7 +41,8 @@
     ;; From https://gist.github.com/huytd/6b785bdaeb595401d69adc7797e5c22c#file-customized-org-mode-theme-el
     (set-face-attribute 'default nil :stipple nil :background "#212121" :foreground "#eeffff" :inverse-video nil
                         ;; :family "Menlo" ;; or Meslo if unavailable: https://github.com/andreberg/Meslo-Font
-                        :family "Hack" ;; brew tap homebrew/cask-fonts && brew cask install font-hack
+                        ;; :family "Hack" ;; brew tap homebrew/cask-fonts && brew cask install font-hack
+                        :family "JetBrains Mono" ;; https://www.jetbrains.com/lp/mono
                         ;; :family "mononoki" ;; https://madmalik.github.io/mononoki/ or sudo apt-get install fonts-mononoki
                         :box nil :strike-through nil :overline nil :underline nil :slant 'normal :weight 'normal
                         :width 'normal :foundry "nil")
