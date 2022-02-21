f604b03e7def968f16b9b52f3a486b92089dd9cd
xenodium <me+gh@xenodium.com>
2022-02-21 18:47:27 +0000

    Adding ar/counsel-apple-search

--- emacs/ar/ar-ios-sim.el
+++ emacs/ar/ar-ios-sim.el
@@ -1,4 +1,4 @@
-;;; ar-ios-sim.el --- iOS simulator support.
+;;; ar-ios-sim.el -*- lexical-binding: t; -*-
 
 ;;; Commentary:
 ;; iOS simulator helpers.
@@ -57,6 +57,44 @@
     (when (> (length bundle-id) 0)
       (setq ar/ios-sim--last-bundle-id bundle-id))
     (find-file path)))
+
+(defun ar/counsel-apple-search ()
+  "Ivy interface for dynamically querying apple.com docs."
+  (interactive)
+  (require 'request)
+  (require 'json)
+  (ivy-read "apple docs: "
+            (lambda (input)
+              (let* ((url (url-encode-url (format "https://developer.apple.com/search/search_data.php?q=%s" input)))
+                     (c1-width (round (* (- (window-width) 9) 0.3)))
+                     (c2-width (round (* (- (window-width) 9) 0.5)))
+                     (c3-width (- (window-width) 9 c1-width c2-width)))
+                (or
+                 (ivy-more-chars)
+                 (let ((request-curl-options (list "-H" (string-trim (url-http-user-agent-string)))))
+                   (request url
+                     :type "GET"
+                     :parser 'json-read
+                     :success (cl-function
+                               (lambda (&key data &allow-other-keys)
+                                 (ivy-update-candidates
+                                  (mapcar (lambda (item)
+                                            (let-alist item
+                                              (propertize
+                                               (format "%s   %s   %s"
+                                                       (truncate-string-to-width (propertize (or .title "")
+                                                                                             'face '(:foreground "yellow")) c1-width nil ?\s "…")
+                                                       (truncate-string-to-width (or .description "") c2-width nil ?\s "…")
+                                                       (truncate-string-to-width (propertize (string-join (or .api_ref_data.languages "") "/")
+                                                                                             'face '(:foreground "cyan1")) c3-width nil ?\s "…"))
+                                               'url .url)))
+                                          (cdr (car data)))))))
+                   0))))
+            :action (lambda (selection)
+                      (browse-url (concat "https://developer.apple.com"
+                                          (get-text-property 0 'url selection))))
+            :dynamic-collection t
+            :caller 'ar/counsel-apple-search))
 
 (provide 'ar-ios-sim)
 
