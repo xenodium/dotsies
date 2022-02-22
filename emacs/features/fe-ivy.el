c118a23f98b0a304412e066879d1aa6f2f21f844
xenodium <me+gh@xenodium.com>
2022-02-22 08:25:33 +0000

    Moving ar/counsel-apple-search to fe-ivy

--- emacs/features/fe-ivy.el
+++ emacs/features/fe-ivy.el
@@ -170,24 +170,62 @@ For example:
                  (ivy-more-chars)
                  (let ((request-curl-options (list "-H" (string-trim (url-http-user-agent-string)))))
                    (request
-                    "https://www.hackingwithswift.com/example-code/search"
-                    :type "GET"
-                    :params (list
-                             (cons "search" input))
-                    :parser 'json-read
-                    :success (cl-function
-                              (lambda (&key data &allow-other-keys)
-                                (ivy-update-candidates
-                                 (mapcar (lambda (item)
-                                           (let-alist item
-                                             (propertize .title 'url .url)))
-                                         data)))))
+                     "https://www.hackingwithswift.com/example-code/search"
+                     :type "GET"
+                     :params (list
+                              (cons "search" input))
+                     :parser 'json-read
+                     :success (cl-function
+                               (lambda (&key data &allow-other-keys)
+                                 (ivy-update-candidates
+                                  (mapcar (lambda (item)
+                                            (let-alist item
+                                              (propertize .title 'url .url)))
+                                          data)))))
                    0)))
               :action (lambda (selection)
                         (browse-url (concat "https://www.hackingwithswift.com"
                                             (get-text-property 0 'url selection))))
               :dynamic-collection t
               :caller 'ar/counsel-hacking-with-swift-search))
+
+  (defun ar/counsel-apple-search ()
+    "Ivy interface for dynamically querying apple.com docs."
+    (interactive)
+    (require 'request)
+    (require 'json)
+    (ivy-read "apple docs: "
+              (lambda (input)
+                (let* ((url (url-encode-url (format "https://developer.apple.com/search/search_data.php?q=%s" input)))
+                       (c1-width (round (* (- (window-width) 9) 0.3)))
+                       (c2-width (round (* (- (window-width) 9) 0.5)))
+                       (c3-width (- (window-width) 9 c1-width c2-width)))
+                  (or
+                   (ivy-more-chars)
+                   (let ((request-curl-options (list "-H" (string-trim (url-http-user-agent-string)))))
+                     (request url
+                       :type "GET"
+                       :parser 'json-read
+                       :success (cl-function
+                                 (lambda (&key data &allow-other-keys)
+                                   (ivy-update-candidates
+                                    (mapcar (lambda (item)
+                                              (let-alist item
+                                                (propertize
+                                                 (format "%s   %s   %s"
+                                                         (truncate-string-to-width (propertize (or .title "")
+                                                                                               'face '(:foreground "yellow")) c1-width nil ?\s "…")
+                                                         (truncate-string-to-width (or .description "") c2-width nil ?\s "…")
+                                                         (truncate-string-to-width (propertize (string-join (or .api_ref_data.languages "") "/")
+                                                                                               'face '(:foreground "cyan1")) c3-width nil ?\s "…"))
+                                                 'url .url)))
+                                            (cdr (car data)))))))
+                     0))))
+              :action (lambda (selection)
+                        (browse-url (concat "https://developer.apple.com"
+                                            (get-text-property 0 'url selection))))
+              :dynamic-collection t
+              :caller 'ar/counsel-apple-search))
 
   ;; Smex handles M-x command sorting. Bringing recent commands to the top.
   (use-package smex
@@ -342,9 +380,9 @@ dash-apple-api://load?request_key=hsM5TRxINf#<dash_entry_language=swift><dash_en
                 (let ((values (s-split "=" item)))
                   (cons (nth 0 values) (nth 1 values))))
               (thread-last (nth 1 (s-split "#" query))
-                (s-chop-suffix ">")
-                (s-chop-prefix "<")
-                (s-split "><")))))
+                           (s-chop-suffix ">")
+                           (s-chop-prefix "<")
+                           (s-split "><")))))
 
 
   (defun adviced:dash-docs-result-url (orig-fun &rest r)
