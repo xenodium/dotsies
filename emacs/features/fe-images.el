72ae0cca2201527e829177fe362595685b6c96c8
xenodium <me+gh@xenodium.com>
2022-03-05 10:50:16 +0000

    Enable opening webp format

--- emacs/features/fe-images.el
+++ emacs/features/fe-images.el
@@ -1,5 +1,10 @@
 ;;; -*- lexical-binding: t; -*-
 
+(use-package image
+  :validate-custom
+  ;; Enable converting external formats (ie. webp) to internal ones.
+  (image-use-external-converter t))
+
 (use-package ar-image
   ;; Generate and open an HTML page with all images in current directory.
   ;; Much nicer for browsing pictures.
