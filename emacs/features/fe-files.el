86d8d39658e1d11a28f10487952b5b1ed024d141
xenodium <me+gh@xenodium.com>
2022-05-06 16:58:34 +0100

    Making ar/files-create-non-existent-directory more robust

--- emacs/features/fe-files.el
+++ emacs/features/fe-files.el
@@ -26,7 +26,8 @@
   :config
   (defun ar/files-create-non-existent-directory ()
     "Create a non-existent directory."
-    (when-let ((parent-directory (file-name-directory buffer-file-name)))
+    (when-let* ((file-name buffer-file-name)
+                (parent-directory (file-name-directory file-name)))
       (when (and (not (file-exists-p parent-directory))
                  (y-or-n-p (format "Create `%s' dir? " parent-directory)))
         (make-directory parent-directory t))))
