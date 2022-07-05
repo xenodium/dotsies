25236cb2719ae72a027b083a468c2391a07e9bc5
xenodium <me+gh@xenodium.com>
2022-07-05 19:55:14 +0100

    Renaming dired-this to dired-script

--- emacs/ar/dired-script.el
+++ emacs/ar/dired-script.el
@@ -1,15 +1,15 @@
 ;;; -*- lexical-binding: t; -*-
 
-(require 'dired-aux)
 (require 'cl-lib)
-(require 'seq)
-(require 'dired)
 (require 'comint)
+(require 'dired)
+(require 'dired-aux)
+(require 'seq)
 
-(defvar ddt--execs nil "All execs in progress")
+(defvar dired-script--execs nil "All execs in progress")
 
 (cl-defstruct
-    ddt--exec
+    dired-script--exec
   "Describes an exec in progress."
   script
   process
@@ -17,24 +17,24 @@
   calling-buffer
   reporter)
 
-(defun ddt-convert-to-gif ()
+(defun dired-script-convert-to-gif ()
   (interactive)
-  (ddt--dired-execute-script-on-marked-files
+  (dired-script--dired-execute-script-on-marked-files
    "ffmpeg -loglevel quiet -stats -y -i ${f} -pix_fmt rgb24 -r 15 ${fne}.gif"
    "Convert to gif" '("ffmpeg")))
 
-(defun ddt-drop-audio ()
+(defun dired-script-drop-audio ()
   (interactive)
-  (ddt--dired-execute-script-on-marked-files
+  (dired-script--dired-execute-script-on-marked-files
    "ffmpeg -i ${f} -c copy -an ${fne}_no_audio.${e}"
    "Remove audio" '("ffmpeg")))
 
-(defun ddt--dired-execute-script-on-marked-files (script name utils)
+(defun dired-script--dired-execute-script-on-marked-files (script name utils)
   "Execute SCRIPT, using buffer NAME, FILES, and bin UTILS."
   (cl-assert (equal major-mode 'dired-mode) nil "Not in dired-mode")
-  (ddt-execute-script script name (dired-get-marked-files) utils))
+  (dired-script-execute-script script name (dired-get-marked-files) utils))
 
-(defun ddt-execute-script (script name files utils)
+(defun dired-script-execute-script (script name files utils)
   "Execute SCRIPT, using buffer NAME, FILES, and bin UTILS."
   (cl-assert (not (string-empty-p script)) nil "Script must not be empty")
   (cl-assert name nil "Script must have a name")
@@ -45,7 +45,7 @@
     (if (seq-empty-p files)
         (setq script template)
       (seq-do (lambda (file)
-                (setq script (concat script "\n" (ddt--expand template file))))
+                (setq script (concat script "\n" (dired-script--expand template file))))
               files))
     (setq script (string-trim script))
     (seq-do (lambda (util)
@@ -76,18 +76,18 @@
               (unless (equal (process-buffer proc)
                              (window-buffer (selected-window)))
                 (kill-buffer (process-buffer proc))))))
-      (setq ddt--execs
+      (setq dired-script--execs
             (push (cons (process-name proc)
-                        (make-ddt--exec :script script
+                        (make-dired-script--exec :script script
                                         :process proc
                                         :name (process-name proc)
                                         :calling-buffer (current-buffer)
                                         :reporter (make-progress-reporter (process-name proc))))
-                  ddt--execs))
-      (set-process-sentinel proc #'ddt--sentinel)
-      (set-process-filter proc #'ddt--filter))))
+                  dired-script--execs))
+      (set-process-sentinel proc #'dired-script--sentinel)
+      (set-process-filter proc #'dired-script--filter))))
 
-(defun ddt--expand (template file)
+(defun dired-script--expand (template file)
   "Expand TEMPLATE. FIXME."
   (setq file (expand-file-name file))
   ;; "${f}" with "/path/file.jpg" -> "'/path/file.jpg'"
@@ -104,23 +104,23 @@
                                           (file-name-sans-extension file)) nil nil template 1)))
   template)
 
-;; (ddt--expand "someutil ${f} -flag" "/path/to/tmp.txt")
-;; (ddt--expand "someutil ${fne}_another.${e} -flag" "/hom/tmp.txt")
-;; (ddt--expand "someutil ${fne}.gif -flag" "/hom/tmp.txt")
+;; (dired-script--expand "someutil ${f} -flag" "/path/to/tmp.txt")
+;; (dired-script--expand "someutil ${fne}_another.${e} -flag" "/hom/tmp.txt")
+;; (dired-script--expand "someutil ${fne}.gif -flag" "/hom/tmp.txt")
 
-(defun ddt--filter (process string)
-  (when-let* ((exec (map-elt ddt--execs (process-name process)))
-              (reporter (ddt--exec-reporter exec)))
+(defun dired-script--filter (process string)
+  (when-let* ((exec (map-elt dired-script--execs (process-name process)))
+              (reporter (dired-script--exec-reporter exec)))
     (progress-reporter-update reporter))
   (comint-output-filter process string))
 
-(defun ddt--sentinel (process state)
-  (let ((exec (map-elt ddt--execs (process-name process))))
+(defun dired-script--sentinel (process state)
+  (let ((exec (map-elt dired-script--execs (process-name process))))
     (when exec
-      (progress-reporter-done (ddt--exec-reporter exec)))
+      (progress-reporter-done (dired-script--exec-reporter exec)))
     (if (= (process-exit-status process) 0)
         (progn
-          (with-current-buffer (ddt--exec-calling-buffer exec)
+          (with-current-buffer (dired-script--exec-calling-buffer exec)
             (when revert-buffer-function)
             (funcall revert-buffer-function))
           (unless (equal (process-buffer process)
@@ -129,5 +129,7 @@
       (if (y-or-n-p (format "Couldn't run %s, see output? " (buffer-name (process-buffer process))))
           (switch-to-buffer (process-buffer process))
         (kill-buffer (process-buffer process)))))
-  (setq ddt--execs
-        (map-delete ddt--execs (process-name process))))
+  (setq dired-script--execs
+        (map-delete dired-script--execs (process-name process))))
+
+(provide 'dired-script)
