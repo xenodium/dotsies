0338e151aa62343d722e2951299c2c0bb171c630
xenodium <me+gh@xenodium.com>
2022-07-06 09:51:59 +0100

    Renaming dired-script to dwim-shell

--- emacs/ar/dwim-shell.el
+++ emacs/ar/dwim-shell.el
@@ -6,10 +6,10 @@
 (require 'dired-aux)
 (require 'seq)
 
-(defvar dired-script--execs nil "All execs in progress")
+(defvar dwim-shell--execs nil "All execs in progress")
 
 (cl-defstruct
-    dired-script--exec
+    dwim-shell--exec
   "Describes an exec in progress."
   script
   process
@@ -19,60 +19,60 @@
   on-completion
   files-before)
 
-(defun dired-script-convert-audio-to-mp3 ()
+(defun dwim-shell-convert-audio-to-mp3 ()
   "Convert all marked audio to mp3(s)."
   (interactive)
-  (dired-script--dired-execute-script-on-marked-files
+  (dwim-shell--dired-execute-script-on-marked-files
    "ffmpeg -stats -n -i <<f>> -acodec libmp3lame <<fne>>.mp3"
    "Convert to mp3" '("ffmpeg")))
 
-(defun dired-script-convert-image-to-jpg ()
+(defun dwim-shell-convert-image-to-jpg ()
   "Convert all marked images to jpg(s)."
   (interactive)
-  (dired-script--dired-execute-script-on-marked-files
+  (dwim-shell--dired-execute-script-on-marked-files
    "convert -verbose <<f>> <<fne>>.jpg"
    "Convert to jpg" '("convert")))
 
-(defun dired-script-convert-image-to-png ()
+(defun dwim-shell-convert-image-to-png ()
   "Convert all marked images to png(s)."
   (interactive)
-  (dired-script--dired-execute-script-on-marked-files
+  (dwim-shell--dired-execute-script-on-marked-files
    "convert -verbose <<f>> <<fne>>.png"
    "Convert to png" '("convert")))
 
-(defun dired-script-convert-to-gif ()
+(defun dwim-shell-convert-to-gif ()
   "Convert all marked videos to optimized gif(s)."
   (interactive)
-  (dired-script--dired-execute-script-on-marked-files
+  (dwim-shell--dired-execute-script-on-marked-files
    "ffmpeg -loglevel quiet -stats -y -i <<f>> -pix_fmt rgb24 -r 15 <<fne>>.gif"
    "Convert to gif" '("ffmpeg")))
 
-(defun dired-script-convert-to-optimized-gif ()
+(defun dwim-shell-convert-to-optimized-gif ()
   "Convert all marked videos to optimized gif(s)."
   (interactive)
-  (dired-script--dired-execute-script-on-marked-files
+  (dwim-shell--dired-execute-script-on-marked-files
    "ffmpeg -loglevel quiet -stats -y -i <<f>> -pix_fmt rgb24 -r 15 <<fne>>.gif
     gifsicle -O3 <<fne>>.gif --lossy=80 -o <<fne>>.gif"
    "Convert to optimized gif" '("ffmpeg" "gifsicle")))
 
-(defun dired-script-unzip ()
+(defun dwim-shell-unzip ()
   "Unzip all marked archives (of any kind) using `atool'."
   (interactive)
-  (dired-script--dired-execute-script-on-marked-files
+  (dwim-shell--dired-execute-script-on-marked-files
    "atool --extract --explain <<f>>" "Unzip" '("atool")))
 
-(defun dired-script-speed-up-gif ()
+(defun dwim-shell-speed-up-gif ()
   "Speeds up gif(s)."
   (interactive)
   (let ((factor (string-to-number
                  (completing-read "Speed up x times: " '("1" "1.5" "2" "2.5" "3" "4")))))
-    (dired-script--dired-execute-script-on-marked-files
+    (dwim-shell--dired-execute-script-on-marked-files
      (format "gifsicle -U <<f>> <<frames>> -O2 -o <<fne>>_x%s.<<e>>" factor)
      "Speed up gif" '("gifsicle" "identify")
      (lambda (script file)
-       (string-replace "<<frames>>" (dired-script--gifsicle-frames-every factor file) script)))))
+       (string-replace "<<frames>>" (dwim-shell--gifsicle-frames-every factor file) script)))))
 
-(defun dired-script--gifsicle-frames-every (skipping-every file)
+(defun dwim-shell--gifsicle-frames-every (skipping-every file)
   (string-join
    (seq-map (lambda (n) (format "'#%d'" n))
             (number-sequence 0 (string-to-number
@@ -80,20 +80,20 @@
                                 (seq-first (process-lines "identify" "-format" "%n\n" file)))
                              skipping-every)) " "))
 
-(defun dired-script-drop-video-audio ()
+(defun dwim-shell-drop-video-audio ()
   "Drop audio from all marked videos."
   (interactive)
-  (dired-script--dired-execute-script-on-marked-files
+  (dwim-shell--dired-execute-script-on-marked-files
    "ffmpeg -i <<f>> -c copy -an <<fne>>_no_audio.<<e>>"
    "Drop audio" '("ffmpeg")))
 
-(defun dired-script--dired-execute-script-on-marked-files (script name utils &optional post-process-template on-completion)
+(defun dwim-shell--dired-execute-script-on-marked-files (script name utils &optional post-process-template on-completion)
   "Execute SCRIPT, using buffer NAME, FILES, and bin UTILS."
   (cl-assert (equal major-mode 'dired-mode) nil "Not in dired-mode")
-  (dired-script-execute-script script name (dired-get-marked-files) utils
+  (dwim-shell-execute-script script name (dired-get-marked-files) utils
                                post-process-template on-completion))
 
-(defun dired-script--dired-files ()
+(defun dwim-shell--dired-files ()
   "List of files in current dired buffer."
   (cl-assert (equal major-mode 'dired-mode) nil "Not in dired-mode")
   (save-excursion
@@ -104,7 +104,7 @@
           (push filename r)))
       (nreverse r))))
 
-(defun dired-script-execute-script (script name files utils &optional post-process-template on-completion)
+(defun dwim-shell-execute-script (script name files utils &optional post-process-template on-completion)
   "Execute SCRIPT, using buffer NAME, FILES, and bin UTILS."
   (cl-assert (not (string-empty-p script)) nil "Script must not be empty")
   (cl-assert name nil "Script must have a name")
@@ -118,7 +118,7 @@
       (seq-do (lambda (file)
                 (setq script
                       (concat script "\n"
-                              (dired-script--expand template file post-process-template))))
+                              (dwim-shell--expand template file post-process-template))))
               files))
     (setq script (string-trim script))
     (seq-do (lambda (util)
@@ -130,7 +130,7 @@
       (shell-command-save-pos-or-erase)
       (view-mode +1)
       (setq view-exit-action 'kill-buffer))
-    (setq files-before (dired-script--dired-files))
+    (setq files-before (dwim-shell--dired-files))
     (setq proc (start-process (buffer-name proc-buffer) proc-buffer "zsh" "-x" "-c" script))
     (if (equal (process-status proc) 'exit)
         (progn
@@ -141,7 +141,7 @@
                 (with-current-buffer (current-buffer)
                   (when revert-buffer-function
                     (funcall revert-buffer-function))
-                  (when-let* ((files-after (dired-script--dired-files))
+                  (when-let* ((files-after (dwim-shell--dired-files))
                               (oldest-new-file (car (last (seq-sort #'file-newer-than-file-p
                                                                     (seq-difference files-after
                                                                                     files-before))))))
@@ -157,19 +157,19 @@
       (with-current-buffer proc-buffer
         (require 'shell)
         (shell-mode))
-      (setq dired-script--execs
+      (setq dwim-shell--execs
             (push (cons (process-name proc)
-                        (make-dired-script--exec :script script
+                        (make-dwim-shell--exec :script script
                                                  :process proc
                                                  :name (process-name proc)
                                                  :calling-buffer (current-buffer)
-                                                 :files-before (dired-script--dired-files)
+                                                 :files-before (dwim-shell--dired-files)
                                                  :reporter (make-progress-reporter (process-name proc))))
-                  dired-script--execs))
-      (set-process-sentinel proc #'dired-script--sentinel)
-      (set-process-filter proc #'dired-script--filter))))
+                  dwim-shell--execs))
+      (set-process-sentinel proc #'dwim-shell--sentinel)
+      (set-process-filter proc #'dwim-shell--filter))))
 
-(defun dired-script--expand (template file &optional post-process-template)
+(defun dwim-shell--expand (template file &optional post-process-template)
   "Expand TEMPLATE, using <<f>> for FILE, <<fne>> for FILE without
  extension, and <<e>> for FILE extension."
   (setq file (expand-file-name file))
@@ -193,35 +193,35 @@
     (setq template (funcall post-process-template template file)))
   template)
 
-(defun dired-script--filter (process string)
-  (when-let* ((exec (map-elt dired-script--execs (process-name process)))
-              (reporter (dired-script--exec-reporter exec)))
+(defun dwim-shell--filter (process string)
+  (when-let* ((exec (map-elt dwim-shell--execs (process-name process)))
+              (reporter (dwim-shell--exec-reporter exec)))
     (progress-reporter-update reporter))
   (comint-output-filter process string))
 
-(defun dired-script--sentinel (process state)
-  (let ((exec (map-elt dired-script--execs (process-name process))))
+(defun dwim-shell--sentinel (process state)
+  (let ((exec (map-elt dwim-shell--execs (process-name process))))
     (when exec
-      (progress-reporter-done (dired-script--exec-reporter exec)))
+      (progress-reporter-done (dwim-shell--exec-reporter exec)))
     (if (= (process-exit-status process) 0)
         (progn
-          (with-current-buffer (dired-script--exec-calling-buffer exec)
+          (with-current-buffer (dwim-shell--exec-calling-buffer exec)
             (when revert-buffer-function
               (funcall revert-buffer-function))
-            (when-let* ((before (dired-script--exec-files-before exec))
-                        (after (dired-script--dired-files))
+            (when-let* ((before (dwim-shell--exec-files-before exec))
+                        (after (dwim-shell--dired-files))
                         (oldest-new-file (car (last (seq-sort #'file-newer-than-file-p
                                                               (seq-difference after before))))))
               (dired-goto-file oldest-new-file)))
-          (when (dired-script--exec-on-completion exec)
-            (funcall (dired-script--exec-on-completion exec)))
+          (when (dwim-shell--exec-on-completion exec)
+            (funcall (dwim-shell--exec-on-completion exec)))
           (unless (equal (process-buffer process)
                          (window-buffer (selected-window)))
             (kill-buffer (process-buffer process))))
       (if (y-or-n-p (format "Couldn't run %s, see output? " (buffer-name (process-buffer process))))
           (switch-to-buffer (process-buffer process))
         (kill-buffer (process-buffer process)))))
-  (setq dired-script--execs
-        (map-delete dired-script--execs (process-name process))))
+  (setq dwim-shell--execs
+        (map-delete dwim-shell--execs (process-name process))))
 
-(provide 'dired-script)
+(provide 'dwim-shell)
