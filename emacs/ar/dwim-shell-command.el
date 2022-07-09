34595e7b992ec3e7c6175c3d6a5ba04de7390650
xenodium <me+gh@xenodium.com>
2022-07-09 19:56:55 +0100

    Fixing path typos

--- emacs/ar/dwim-shell-command.el
+++ emacs/ar/dwim-shell-command.el
@@ -110,16 +110,16 @@
   (dwim-shell-command--on-marked-files
    "Convert png to icns icon"
    "mkdir <<fne>>.iconset
-sips -z 16 16 <<f>> --out <<fne>>.iconset/icon16x16.png
-sips -z 32 32 <<f>> --out <<fne>>.iconset/icon16x16@2x.png
-sips -z 32 32 <<f>> --out <<fne>>.iconset/icon32x32.png
-sips -z 64 64 <<f>> --out <<fne>>.iconset/icon32x32@2x.png
-sips -z 128 128 <<f>> --out <<fne>>.iconset/icon128x128.png
-sips -z 256 256 <<f>> --out <<fne>>.iconset/icon128x128@2x.png
-sips -z 256 256 <<f>> --out <<fne>>.iconset/icon256x256@2x.png
-sips -z 512 512 <<f>> --out <<fne>>.iconset/icon512x512.png
-sips -z 512 512 <<f>> --out <<fne>>.iconset/icon256x256@2x.png
-sips -z 1024 1024 <<f>> --out <<fne>>.iconset/icon512x512@2x.png
+sips -z 16 16 <<f>> --out <<fne>>.iconset/icon_16x16.png
+sips -z 32 32 <<f>> --out <<fne>>.iconset/icon_16x16@2x.png
+sips -z 32 32 <<f>> --out <<fne>>.iconset/icon_32x32.png
+sips -z 64 64 <<f>> --out <<fne>>.iconset/icon_32x32@2x.png
+sips -z 128 128 <<f>> --out <<fne>>.iconset/icon_128x128.png
+sips -z 256 256 <<f>> --out <<fne>>.iconset/icon_128x128@2x.png
+sips -z 256 256 <<f>> --out <<fne>>.iconset/icon_256x256@2x.png
+sips -z 512 512 <<f>> --out <<fne>>.iconset/icon_512x512.png
+sips -z 512 512 <<f>> --out <<fne>>.iconset/icon_256x256@2x.png
+sips -z 1024 1024 <<f>> --out <<fne>>.iconset/icon_512x512@2x.png
 iconutil -c icns <<fne>>.iconset
 "
    :utils '("sips" "iconutil")
