3721410848e5904635fd7bf8807dcb285dbe0651
xenodium <me+gh@xenodium.com>
2022-07-13 20:40:15 +0100

    Deleting unused code

--- emacs/features/fe-info.el
+++ emacs/features/fe-info.el
@@ -1,10 +1,14 @@
 ;;; -*- lexical-binding: t; -*-
 
-(use-package ar-info
-  :commands ar/format-info-mode)
-
 (use-package info-look
   :commands (info-lookup-symbol
              info-lookup-maybe-add-help)
   :config
-  (add-to-list 'Info-directory-list "~/.emacs.d/info"))
+  (add-to-list 'Info-directory-list "~/.emacs.d/info")
+
+  (defun ar/format-info-mode ()
+    "Opening .info files does not automatically set things up. Give it a little help."
+    (interactive)
+    (let ((file-name (buffer-file-name)))
+      (kill-buffer (current-buffer))
+      (info file-name))))
