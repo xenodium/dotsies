2bd91dd0d098516c0c73c71b5735aade3452bb19
xenodium <me+gh@xenodium.com>
2022-09-24 19:54:52 +0100

    Cleanup trash usage

--- emacs/features/fe-mac.el
+++ emacs/features/fe-mac.el
@@ -19,12 +19,11 @@
 
   (setq trash-directory "~/.Trash")
 
-  ;; https://www.emacswiki.org/emacs/SystemTrash
   ;; See `trash-directory' as it requires defining `system-move-file-to-trash'.
   (defun system-move-file-to-trash (file)
     "Use \"trash\" to move FILE to the system trash."
     (cl-assert (executable-find "trash") nil "'trash' must be installed. Needs \"brew install trash\"")
-    (call-process (executable-find "trash") nil 0 nil file))
+    (call-process "trash" nil 0 nil "-F"  file))
 
   (use-package ns-win
     ;; Easily insert # on macOS/UK keyboard.
