8084a76a93e34ccb924ecde6a0f2c03424dec420
xenodium <me+gh@xenodium.com>
2023-05-11 12:38:36 +0100

    Make ar/window-state-state-change more resilient

--- emacs/features/fe-files.el
+++ emacs/features/fe-files.el
@@ -45,14 +45,22 @@
   ;; global-auto-revert-mode can slow things down. try to enable it per active window.
   (add-to-list 'window-state-change-functions
                (defun ar/window-state-state-change (state)
-                 (let ((old-buffer (window-buffer (old-selected-window)))
-                       (new-buffer (window-buffer (selected-window))))
-                   (with-current-buffer old-buffer
-                     (when buffer-file-name
-                       (auto-revert-mode -1)))
-                   (with-current-buffer new-buffer
-                     (when buffer-file-name
-                       (auto-revert-mode +1)))))))
+                 (let* ((old-selected-window (old-selected-window))
+                        (old-buffer (when old-selected-window
+                                      (window-buffer old-selected-window)))
+                        (selected-window (selected-window))
+                        (new-buffer (when selected-window
+                                      (window-buffer selected-window))))
+                   (when old-buffer
+                     (with-current-buffer old-buffer
+                       (when buffer-file-name
+                         ;; (message "auto-revert-mode -1 <%s>" buffer-file-name)
+                         (auto-revert-mode -1))))
+                   (when new-buffer
+                     (with-current-buffer new-buffer
+                       (when buffer-file-name
+                         ;; (message "auto-revert-mode +1 <%s>" buffer-file-name)
+                         (auto-revert-mode +1))))))))
 
 ;; Avoid creating lock files (ie. .#some-file.el)
 (setq create-lockfiles nil)
