db20cfd4be56215dc012059cc7dbf91b37118b9b
xenodium <me+gh@xenodium.com>
2023-09-05 11:47:21 +0100

    Adds inline edebug results

--- emacs/features/fe-elisp.el
+++ emacs/features/fe-elisp.el
@@ -100,11 +100,34 @@
   ;; TODO: Figure out why face foreground isn't displayed.
   (set-face-attribute 'easy-escape-face nil :foreground "red"))
 
+;; Inline evaluation (use M-C-x).
 (use-package eros
-  ;; Inline evaluation (use M-C-x).
   :ensure t
   :commands eros-eval-defun
-  :hook (emacs-lisp-mode . eros-mode))
+  :hook (emacs-lisp-mode . eros-mode)
+  :config
+  (defun adviced:edebug-compute-previous-result (_ &rest r)
+    "Adviced `edebug-compute-previous-result'."
+    (let ((previous-value (nth 0 r)))
+      (if edebug-unwrap-results
+          (setq previous-value
+                (edebug-unwrap* previous-value)))
+      (setq edebug-previous-result
+            (edebug-safe-prin1-to-string previous-value))))
+
+  (advice-add #'edebug-compute-previous-result
+              :around
+              #'adviced:edebug-compute-previous-result)
+
+  (defun adviced:edebug-previous-result (_ &rest r)
+    "Adviced `edebug-previous-result'."
+    (eros--make-result-overlay edebug-previous-result
+      :where (point)
+      :duration eros-eval-result-duration))
+
+  (advice-add #'edebug-previous-result
+              :around
+              #'adviced:edebug-previous-result))
 
 (use-package clojure-mode
   :ensure t
