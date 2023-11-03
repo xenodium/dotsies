4b3e13adcb580c732c2c54b48fd50d9b1d89d49c
xenodium <me+gh@xenodium.com>
2023-11-03 12:02:46 +0000

    Make chatgpt-shell compose accessible from other modes via C-c C-e

--- emacs/features/fe-llm.el
+++ emacs/features/fe-llm.el
@@ -2,13 +2,17 @@
 
 (use-package chatgpt-shell
   :commands
-  chatgpt-shell
+  (chatgpt-shell
+   chatgpt-shell-prompt-compose)
   :validate-custom
   ((chatgpt-shell-openai-key
     (lambda ()
       (nth 0 (process-lines "pass" "show" "openai-key")))))
-  :bind
-  (("C-c C-e" . chatgpt-shell-prompt-compose)))
+  :bind (("C-c C-e" . chatgpt-shell-prompt-compose)
+         :map org-mode-map
+         ("C-c C-e" . chatgpt-shell-prompt-compose)
+         :map emacs-lisp-mode-map
+         ("C-c C-e" . chatgpt-shell-prompt-compose)))
 
 (use-package dall-e-shell
   :validate-custom
