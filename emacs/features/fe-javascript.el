b181b4acac22e7e101faf7f984c44eafc23c046b
xenodium <me+gh@xenodium.com>
2024-02-03 21:24:30 +0000

    Adds mocha compilation buffer error resolution

--- emacs/features/fe-javascript.el
+++ emacs/features/fe-javascript.el
@@ -11,4 +11,13 @@
 (use-package typescript-mode
   :ensure t
   :validate-custom
-  (typescript-indent-level 2))
+  (typescript-indent-level 2)
+  :config
+  ;; Resolve mocha-reported errors
+  (add-to-list 'compilation-error-regexp-alist-alist
+               '(mocha-at ".*(\\([^:]+\\):\\([0-9]+\\):\\([0-9]+\\))"
+                          1 ;; file path
+                          2 ;; line
+                          3 ;; column
+                          ))
+  (add-to-list 'compilation-error-regexp-alist 'mocha-at))
