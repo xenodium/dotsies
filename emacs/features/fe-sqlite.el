05bf6340da3fb789cd7eb80d764cce448545484d
xenodium <me+gh@xenodium.com>
2024-06-19 11:01:58 +0100

    Use sqlite-mode-extras package bindings

--- emacs/features/fe-sqlite.el
+++ emacs/features/fe-sqlite.el
@@ -14,19 +14,4 @@ current buffer, killing it."
   (add-to-list 'magic-mode-alist '("SQLite format 3\x00" . ar/sqlite-redirect-open))
 
   (use-package sqlite-mode-extras
-    :bind (:map
-           sqlite-mode-map
-           ("n" . next-line)
-           ("p" . previous-line)
-           ("b" . sqlite-mode-extras-backtab-dwim)
-           ("f" . sqlite-mode-extras-tab-dwim)
-           ("+" . sqlite-mode-extras-add-row)
-           ("D" . sqlite-mode-extras-delete-row-dwim)
-           ("C" . sqlite-mode-extras-compose-and-execute)
-           ("E" . sqlite-mode-extras-execute)
-           ("S" . sqlite-mode-extras-execute-and-display-select-query)
-           ("DEL" . sqlite-mode-extras-delete-row-dwim)
-           ("g" . sqlite-mode-extras-refresh)
-           ("<backtab>" . sqlite-mode-extras-backtab-dwim)
-           ("<tab>" . sqlite-mode-extras-tab-dwim)
-           ("RET" . sqlite-mode-extras-ret-dwim))))
+    :hook ((sqlite-mode . sqlite-extras-minor-mode))))
