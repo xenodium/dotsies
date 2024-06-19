0b839da41842ac8f9cd3c10bf6ef1d62ed125b39
xenodium <me+gh@xenodium.com>
2024-06-19 11:03:57 +0100

    Opening sqlite files in sqlite-mode

--- emacs/features/fe-sqlite.el
+++ emacs/features/fe-sqlite.el
@@ -7,9 +7,11 @@
     "Runs `sqlite-mode-open-file' on the file name visited by the
 current buffer, killing it."
 
-    (let ((file-name buffer-file-name))
+    (let ((file-name buffer-file-name)
+          (directory default-directory))
       (kill-current-buffer)
-      (sqlite-mode-open-file file-name)))
+      (sqlite-mode-open-file file-name)
+      (setq default-directory directory)))
 
   (add-to-list 'magic-mode-alist '("SQLite format 3\x00" . ar/sqlite-redirect-open))
 
