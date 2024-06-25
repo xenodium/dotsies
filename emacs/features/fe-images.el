3752d9bc7705d7ad8e1e3db7e07ef86e4b3fcfa8
xenodium <me+gh@xenodium.com>
2024-06-25 10:20:46 +0100

    Disable imagemagick usage (no longer needed as of Emacs 29)

--- emacs/features/fe-images.el
+++ emacs/features/fe-images.el
@@ -1,5 +1,12 @@
 ;;; -*- lexical-binding: t; -*-
 
+(use-package image
+  :validate-custom
+  ;; emacs-plus compiles with imagemagick, but is no longer needed to
+  ;; open other formats like webp. Setting imagemagick-types-inhibit
+  ;; disables imagemagick usage.
+  (imagemagick-types-inhibit t))
+
 (use-package ar-image
   ;; Generate and open an HTML page with all images in current directory.
   ;; Much nicer for browsing pictures.
