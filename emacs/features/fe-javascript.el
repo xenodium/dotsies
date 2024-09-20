dcdf9bde732c61120e9dce79936a2627ca61c3bf
xenodium <me+gh@xenodium.com>
2024-09-20 16:31:24 +0100

    Resolve mocha-generated errors

--- emacs/features/fe-javascript.el
+++ emacs/features/fe-javascript.el
@@ -19,9 +19,7 @@
   :config
   ;; Resolve mocha-reported errors
   (add-to-list 'compilation-error-regexp-alist-alist
-               '(mocha-at ".*(\\([^:]+\\):\\([0-9]+\\):\\([0-9]+\\))"
-                          1 ;; file path
-                          2 ;; line
-                          3 ;; column
-                          ))
-  (add-to-list 'compilation-error-regexp-alist 'mocha-at))
+               '(mocha
+                 "file://\\([^:]+\\):\\([0-9]+\\):\\([0-9]+\\)"
+                 1 2 3))
+  (add-to-list 'compilation-error-regexp-alist 'mocha))
