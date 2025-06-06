89b138809e3a216e747ce8a54950bd3a2fbf47cb
xenodium <me+gh@xenodium.com>
2025-06-06 16:50:58 +0100

    Updating LLM shell config

--- emacs/features/fe-llm.el
+++ emacs/features/fe-llm.el
@@ -5,16 +5,38 @@
   (chatgpt-shell
    chatgpt-shell-prompt-compose)
   :validate-custom
-  ((chatgpt-shell-openai-key
+  ((chatgpt-shell-model-version "chatgpt-4o-latest")
+   (chatgpt-shell-openai-key
     (lambda ()
-      (nth 0 (process-lines "pass" "show" "openai-key")))))
+      (nth 0 (process-lines "pass" "show" "openai-key"))))
+   (chatgpt-shell-perplexity-key
+    (lambda ()
+      (nth 0 (process-lines "pass" "show" "perplexity-key"))))
+   (chatgpt-shell-kagi-key
+    (lambda ()
+      (nth 0 (process-lines "pass" "show" "kagi-key"))))
+   (chatgpt-shell-openrouter-key
+    (lambda ()
+      (nth 0 (process-lines "pass" "show" "openrouter-key"))))
+   (chatgpt-shell-google-key
+    (lambda ()
+      (nth 0 (process-lines "pass" "show" "google-key"))))
+   (chatgpt-shell-anthropic-key
+    (lambda ()
+      (nth 0 (process-lines "pass" "show" "anthropic-key")))))
   :bind (("C-c C-e" . chatgpt-shell-prompt-compose)
+         :map smartparens-mode-map
+         (("C-c e" . chatgpt-shell-quick-insert))
+         :map c-mode-map
+         (("C-c e" . chatgpt-shell-quick-insert))
          :map org-mode-map
-         ("C-c C-e" . chatgpt-shell-prompt-compose)
+         (("C-c C-e" . chatgpt-shell-prompt-compose))
+         :map objc-mode-map
+         (("C-c C-e" . chatgpt-shell-prompt-compose))
          :map eshell-mode-map
-         ("C-c C-e" . chatgpt-shell-prompt-compose)
+         (("C-c C-e" . chatgpt-shell-prompt-compose))
          :map emacs-lisp-mode-map
-         ("C-c C-e" . chatgpt-shell-prompt-compose))
+         (("C-c C-e" . chatgpt-shell-prompt-compose)))
   :config
   (add-to-list 'display-buffer-alist
                (cons '(major-mode . chatgpt-shell-prompt-compose-mode)
@@ -25,7 +47,7 @@
                                                     (car (window-at-side-list nil 'left))
                                                     'reuse alist)))
                         display-buffer-in-direction)
-                       (window-width . 0.35)
+                       (window-width . 0.45)
                        (direction . left)))))
 
 (use-package ob-chatgpt-shell
@@ -33,9 +55,13 @@
   :config
   (ob-chatgpt-shell-setup))
 
+(use-package ob-dall-e-shell
+  :commands (org-babel-execute:dall-e-shell)
+  :config
+  (ob-dall-e-shell-setup))
+
 (use-package dall-e-shell
   :validate-custom
-  (dall-e-shell-model-version "dall-e-3")
   (dall-e-shell-openai-key
    (lambda ()
      (nth 0 (process-lines "pass" "show" "openai-key"))))
