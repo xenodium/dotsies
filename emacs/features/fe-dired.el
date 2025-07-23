a183363f20118b7cc527e48b36620034a64e2ec9
xenodium <me+gh@xenodium.com>
2025-07-23 20:44:58 +0100

    Adds dragging and buffer-creation from marked items

--- emacs/features/fe-dired.el
+++ emacs/features/fe-dired.el
@@ -65,7 +65,9 @@
               ("i" . dired-hide-details-mode)
               ("s" . hydra-dired-sort/body)
               ("A" . ar/dired-mark-all)
-              ("M" . ar/dired-mark-all))
+              ("M" . ar/dired-mark-all)
+              ("M-<up>" . ar/dired-drag-item-up)
+              ("M-<down>" . ar/dired-drag-item-down))
   :commands (dired-mode
              dired
              ar/dwim-copy-file-path
@@ -278,6 +280,63 @@ If `universal-argument' is called, copy only the dir path."
       (if (string-match "^/sudo:" dir)
           (user-error "Already in sudo")
         (dired (concat "/sudo::" dir)))))
+
+  (defun ar/dired-drag-item-up ()
+    "Drag dired item down in buffer."
+    (interactive)
+    (unless (dired-get-filename nil t)
+      (error "Not a dired draggable item"))
+    (when (= (line-number-at-pos) 2)
+      (error "Already at top"))
+    (let* ((inhibit-read-only t)
+           (col (current-column))
+           (item-start (line-beginning-position))
+           (item-end (1+ (line-end-position)))
+           (item (buffer-substring item-start item-end)))
+      (delete-region item-start item-end)
+      (forward-line -1)
+      (beginning-of-line)
+      (insert item)
+      (forward-line -1)
+      (move-to-column col)))
+
+  (defun ar/dired-drag-item-down ()
+    "Drag dired item down in buffer."
+    (interactive)
+    (unless (dired-get-filename nil t)
+      (error "Not a dired draggable item"))
+    (when (save-excursion
+            (forward-line 1)
+            (eobp))
+      (error "Already at bottom"))
+    (let* ((inhibit-read-only t)
+           (col (current-column))
+           (item-start (line-beginning-position))
+           (item-end (1+ (line-end-position)))
+           (item (buffer-substring item-start item-end)))
+      (delete-region item-start item-end)
+      (forward-line 1)
+      (beginning-of-line)
+      (insert item)
+      (forward-line -1)
+      (move-to-column col)))
+
+  (defun ar/dired-from-marked-items ()
+    "Create a new dired buffer containing only the marked files.
+
+Also allow dragging items up and down via M-<up> and M-x<down>."
+    (interactive)
+    (let ((marked-files (dired-get-marked-files))
+          (buffer-name (generate-new-buffer-name
+                        (format "*%s (selection)*"
+                                (file-name-nondirectory
+                                 (directory-file-name default-directory))))))
+      (unless marked-files
+        (error "No dired marked files"))
+      (dired (cons buffer-name
+                   (mapcar (lambda (path)
+                             (file-relative-name path default-directory))
+                           marked-files)))))
 
   (use-package dired-filter
     :ensure t
