d13f17a1d30a9b58466dac06de185fcdac1ebc46
xenodium <me+gh@xenodium.com>
2026-02-08 21:21:41 +0000

    Adding ar/toggle-let*

--- emacs/features/fe-elisp.el
+++ emacs/features/fe-elisp.el
@@ -69,6 +69,18 @@
         :where (point)
         :duration eros-eval-result-duration)))
 
+  ;; From https://www.reddit.com/r/emacs/comments/1qyp1rk/toggle_between_let_and_let/
+  (defun ar/toggle-let* ()
+    "Make the closest enclosing let or let* form a let* or let form, respectively,
+then reindent that form's bindings. "
+    (interactive)
+    (save-excursion
+      (while (progn
+               (condition-case nil (backward-up-list nil t) (scan-error (error "Not within a let form")))
+               (not (search-forward-regexp (rx point "(" (* space) "let" (? "*") symbol-end) nil t))))
+      (if (= ?* (char-before)) (backward-delete-char 1) (insert "*"))
+      (indent-region (point) (progn (forward-sexp) (point)))))
+
   (defun ar/eval-uncommented-region (start end)
     (interactive "r")
     (let ((text (buffer-substring-no-properties start end)))
